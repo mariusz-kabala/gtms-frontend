1bcf37956c80745258de8b2849bf3ed7df79779d
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-07-26 16:59:08 +0200

    initial commit, playing around

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+function Home() {
+    return <div>
+        Welcome to Next.js!
+        <p>scoped!</p>
+      <style jsx>{`
+        p {
+          color: blue;
+        }
+        div {
+          background: red;
+        }
+        @media (max-width: 600px) {
+          div {
+            background: blue;
+          }
+        }
+      `}</style>
+      <style global jsx>{`
+        body {
+          background: black;
+        }
+      `}</style>
+    </div>;
+  }
+  
+  export default Home;
