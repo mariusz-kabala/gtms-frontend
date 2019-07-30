afa2ee86a43150925f9752c5d471eddbd440f41d
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-07-30 20:44:03 +0200

    setup fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
-import React from "react"
-import App, { Container, AppContext } from "next/app"
-import RulesProvider from "providers/Rules"
-import { appWithTranslation } from "../i18n"
+import React from 'react'
+import App, { Container } from 'next/app'
+import RulesProvider from 'providers/Rules'
+import { appWithTranslation } from '../i18n'
 
 class GTMSApp extends App {
-  static async getInitialProps({ Component, ctx }: AppContext) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
