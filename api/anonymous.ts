158e587b28577a31df61e09958a6439ebc3dde76
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-08-01 17:30:34 +0200

    feat: auth service first try

--- api/anonymous.ts
+++ api/anonymous.ts
@@ -1,18 +1,29 @@
 import { fetchJSON, makeApiUrl } from 'api/common'
 
 export interface IRegisterUserReqPayload {
-    email: string
-    password: string
-    name?: string
-    confirmPassword: string
+  email: string
+  password: string
+  name?: string
+  confirmPassword: string
 }
 
-export interface ILoginUserReqPayload {
-    email: string
-    password: string
+export interface ILoginRequestPayload {
+  email: string
+  password: string
 }
 
-export const registerUser = (payload: IRegisterUserReqPayload) => fetchJSON<IRegisterUserReqPayload>(makeApiUrl('auth/users'), { values: payload })
+export interface ILoginResponse {
+  accessToken: string
+  refreshToken: string
+}
 
-export const loginUser = (payload: ILoginUserReqPayload) =>
-    fetchJSON<ILoginUserReqPayload>(makeApiUrl('auth/authenticate'), { values: payload })
+export const registerUser = (payload: IRegisterUserReqPayload) =>
+  fetchJSON<IRegisterUserReqPayload, any>(makeApiUrl('auth/users'), {
+    values: payload,
+  })
+
+export const loginUser = (payload: ILoginRequestPayload) =>
+  fetchJSON<ILoginRequestPayload, ILoginResponse>(
+    makeApiUrl('auth/authenticate'),
+    { values: payload }
+  )
--- api/common/index.ts
+++ api/common/index.ts
@@ -1,33 +1,36 @@
 interface IParams<T> {
-    values: T
-    headers?: {
-        [key: string]: string
-    }
+  values: T
+  headers?: {
+    [key: string]: string
+  }
 }
 
-export const fetchJSON = <T>(url: string, params?: IParams<T> ) => {
-    const { values = {}, headers = {} } = params || {}
-    const options: RequestInit = {}
+export const fetchJSON = <T, R>(
+  url: string,
+  params?: IParams<T>
+): Promise<R> => {
+  const { values = {}, headers = {} } = params || {}
+  const options: RequestInit = {}
 
-    if (values instanceof Object && Object.keys(values).length > 0) {
-        headers['Content-Type'] = 'application/json'
+  if (values instanceof Object && Object.keys(values).length > 0) {
+    headers['Content-Type'] = 'application/json'
 
-        options.body = JSON.stringify(values)
-        options.method = 'POST'
-        options.cache = 'no-cache'
-    }
+    options.body = JSON.stringify(values)
+    options.method = 'POST'
+    options.cache = 'no-cache'
+  }
 
-    options.headers = new Headers(headers)
+  options.headers = new Headers(headers)
 
-    return fetch(url, options)
-        .then((response) => {
-            if (response.ok) {
-                return response.json()
-            }
+  return fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
 
-            throw response
-        })
-        .then((data) => data)
+      throw response
+    })
+    .then((data: R) => data)
 }
 
 export const makeApiUrl = (url: string): string => `/api/v1/${url}`
