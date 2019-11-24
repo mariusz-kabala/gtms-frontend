ed4fa7526ed90774c308d2dffac0623ed37b106c
Mariusz <mariusz@kabala.waw.pl>
2019-11-24 06:46:38 +0100

    fix: registration form clean up - translations and types

--- pages/Registration/components/Form/index.tsx
+++ pages/Registration/components/Form/index.tsx
@@ -4,42 +4,69 @@ import { NFC } from 'types/nfc.d'
 import { useTranslation } from 'i18n'
 import classNames from './styles.scss'
 
+interface IRegistrationPayload {
+  email: string
+  username?: string
+  password: string
+  passwordConfirmation: string
+}
+
 export const RegistrationForm: NFC<{}> = () => {
   const { t } = useTranslation('registration')
-  const { register, handleSubmit, errors } = useForm()
-  const onSubmit = (data: any) => console.log(data)
+  const { register, handleSubmit, errors, setError } = useForm()
+  const onSubmit = (data: any) => {
+    const { password, passwordConfirmation } = data
+
+    if (password !== passwordConfirmation) {
+      setError(
+        'passwordConfirmation',
+        'notMatch',
+        t('form.validation.passwordConfirmation.notMatch')
+      )
+      return
+    }
+
+    console.log(data)
+  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit()}>
         <div className={classNames.item}>
-          <label htmlFor="email">{t('form.email')}</label>
+          <label htmlFor="email">{t('form.labels.email')}</label>
           <input
             type="email"
             name="email"
             id="email"
             ref={register({ required: true })}
           />
-          {errors.email && <span>{t('emailIsRequired')}</span>}
+          {errors.email && (
+            <span className={classNames.error}>
+              {t('form.validation.email.isRequired')}
+            </span>
+          )}
         </div>
         <div className={classNames.item}>
-          <label htmlFor="username">{t('form.username')}</label>
+          <label htmlFor="username">{t('form.labels.username')}</label>
           <input type="text" name="username" id="username" ref={register} />
-          {errors.email && <span>{t('emailIsRequired')}</span>}
         </div>
         <div className={classNames.item}>
-          <label htmlFor="password">{t('form.password')}</label>
+          <label htmlFor="password">{t('form.labels.password')}</label>
           <input
             type="password"
             name="password"
             id="password"
             ref={register({ required: true })}
           />
-          {errors.password && <span>{t('passwordIsRequired')}</span>}
+          {errors.password && (
+            <span className={classNames.error}>
+              {t('form.validation.password.isRequired')}
+            </span>
+          )}
         </div>
         <div className={classNames.item}>
           <label htmlFor="passwordConfirmation">
-            {t('form.passwordConfirmation')}
+            {t('form.labels.passwordConfirmation')}
           </label>
           <input
             type="password"
@@ -48,12 +75,14 @@ export const RegistrationForm: NFC<{}> = () => {
             ref={register({ required: true })}
           />
           {errors.passwordConfirmation && (
-            <span>{t('passwordConfirmationIsRequired')}</span>
+            <span className={classNames.error}>
+              {t('form.validation.passwordConfirmation.isRequired')}
+            </span>
           )}
         </div>
         <div>
           <button type="submit" disabled={false} className={classNames.button}>
-            {t('submitButton')}
+            {t('form.submitButton')}
           </button>
         </div>
       </form>
