880900486714fbee9643fca0325cc974bad3e62f
ra8ga <rafal.furmaga@gmail.com>
2019-11-27 17:30:35 +0100

    Update index.tsx

--- components/common/Overlay/index.tsx
+++ components/common/Overlay/index.tsx
@@ -2,5 +2,5 @@ import React, { FC } from 'react'
 import styles from './styles.scss'
 
 export const Overlay: FC<{ onClick: () => unknown }> = ({ onClick }) => (
-  <div onClick={() => onClick()} className={styles.overlay} />
+  <div onClick={onClick} className={styles.overlay} />
 )
