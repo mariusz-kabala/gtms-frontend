8d798c4baae2c8146ac032a16ec4fc7b62560afc
rafal <rafal@rafal.comm>
2019-11-27 12:47:48 +0100

    feat: overlay component

---
+++ components/common/Overlay/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+
+export const Overlay: FC<{ onClick: () => unknown }> = ({ onClick }) => (
+  <div onClick={() => onClick()} className={styles.overlay} />
+)
