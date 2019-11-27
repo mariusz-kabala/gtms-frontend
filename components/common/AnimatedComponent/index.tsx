51693f00d20b9d71c60a698825b51bb8eab2bb9f
rafal <rafal@rafal.comm>
2019-11-27 18:58:02 +0100

    add: feat animated component

---
+++ components/common/AnimatedComponent/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from 'react'
+import styles from './styles.scss'
+import cx from 'classnames'
+
+export const AnimatedComponent: FC<{
+  additionalStyles?: string
+  children: ReactNode
+  infinite: boolean
+  onClick: () => unknown
+}> = ({ additionalStyles, infinite, children }) => (
+  <div className={styles.animated}>
+    <div
+      className={cx(styles.content, additionalStyles, {
+        [styles.infinite]: infinite,
+      })}
+    >
+      {children}
+    </div>
+  </div>
+)
