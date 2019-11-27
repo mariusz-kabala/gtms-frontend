c91a8dfda3c5f9febf5ce510d3a176a20413375b
rafal <rafal@rafal.comm>
2019-11-27 19:19:51 +0100

    feat: logo component

--- components/common/Logo/index.tsx
+++ components/common/Logo/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import styles from './styles.scss'
-import LogoImageRectangle from 'assets/images/logo_bankpodrozy_rectangle.png'
+import LogoImageRectangle from 'static/images/logo_bankpodrozy_rectangle.png'
 
 export const Logo: FC<{}> = () => (
   <div className={styles.logo}>
