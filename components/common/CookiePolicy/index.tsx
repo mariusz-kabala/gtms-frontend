0279dfb2f1bc5c361b223b97321ab47ba94b2315
rafal <rafal@rafal.comm>
2019-11-27 20:04:39 +0100

    feat: cookie policy

--- components/common/CookiePolicy/index.tsx
+++ components/common/CookiePolicy/index.tsx
@@ -1,26 +1,26 @@
 import React, { FC, ReactNode } from 'react'
 import styles from './styles.scss'
 import { getItem, setItem } from 'helpers/localStorage'
-import Button from 'components/Button'
+import { Button } from 'components/common/Button'
 
-export const CookiePolicy: FC<{ children: ReactNode }> = ({ children }) => {
-  const isCookieAccepted = getItem('isCookieAccepted')
+export const CookiePolicy: FC<{ children: ReactNode }> = ({ children }) => (
+  <div />
+)
+// const isCookieAccepted = getItem('isCookieAccepted')
 
-  return isCookieAccepted === 'true' ? (
-    children
-  ) : (
-    <div className={styles.cookiePolicy}>
-      <div className={styles.cookie}>
-        <p>here goes cookie policy</p>
-        <Button
-          onClick={() => {
-            setItem('isCookieAccepted', 'true')
-          }}
-        >
-          Accept
-        </Button>
-      </div>
-      {children}
-    </div>
-  )
-}
+// return isCookieAccepted === 'true' ? children : (
+//   <div className={styles.cookiePolicy}>
+//     <div className={styles.cookie}>
+//       <p>here goes cookie policy</p>
+//       <Button
+//         onClick={() => {
+//           setItem('isCookieAccepted', 'true')
+//         }}
+//       >
+//         Accept
+//       </Button>
+//     </div>
+//     {children}
+//   </div>
+// )
+// }
