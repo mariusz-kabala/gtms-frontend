f5e6510025570257cc1a25e45e8f87d4410e21f2
ra8ga <rafal.furmaga@gmail.com>
2019-11-27 19:35:57 +0100

    feat: promotedTags and spinner component (#19)
    * feat: promotedTags component
    
    * feat: spinner
    
    * refactor: CR changes

---
+++ components/common/PromotedTags/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+import { Tag } from 'components/common/Tag'
+import { TagGroup } from 'components/common/TagGroup'
+
+export const PromotedTags: FC<{}> = () => (
+  <div className={styles.promotedTags}>
+    <TagGroup additionalStyles={styles.promotedTags}>
+      {/* @todo remove mock data */}
+      <Tag label="Mechanik" />
+      <Tag label="Oddam" />
+      <Tag label="SerwisRowerowy" />
+      <Tag label="Impreza" />
+      <Tag label="DzienKobiet" />
+      <Tag label="Znaleziono" />
+      <Tag label="Polityka" />
+    </TagGroup>
+  </div>
+)
---
+++ components/common/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+
+export const Spinner: FC<{}> = () => (
+  <div className={styles.spinner}>
+    <div className={styles.rect1} />
+    <div className={styles.rect2} />
+    <div className={styles.rect3} />
+    <div className={styles.rect4} />
+    <div className={styles.rect5} />
+  </div>
+)
