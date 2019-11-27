b349d39aa0a4b518363c5e94f620b7235d87d658
ra8ga <rafal.furmaga@gmail.com>
2019-11-27 18:14:16 +0100

    feat: introduce usercard component (#18)
    * feat: introduce usercard component
    
    * fix: remove unneeded var

---
+++ components/common/UserCard/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+
+export const UserCard: FC<{
+  image: string
+}> = ({ image }) => (
+  <div className={styles.container}>
+    {/* @todo add translations when needed */}
+    <img className={styles.avatar} src={image} alt="user avatar" />
+    <div className={styles.content}>
+      <span className={styles.nameSurname}>Johnny Silverhand</span>
+      <p className={styles.desc}>
+        Id in veniam sunt labore. Adipisicing proident dolor nulla cillum
+        cupidatat. Do sint labore cupidatat commodo veniam tempor tempor
+        exercitation commodo aute nostrud nostrud.
+      </p>
+    </div>
+  </div>
+)
