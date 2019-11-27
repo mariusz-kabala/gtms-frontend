adaeab61c44e1359ade6c452488644c1fd7e339e
rafal <rafal@rafal.comm>
2019-11-27 19:44:13 +0100

    feat: modal

---
+++ components/common/Modal/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+import cx from 'classnames'
+import Overlay from 'components/Overlay'
+
+export const Modal: FC<{
+  additionalStyles?: string
+  children: ReactNode
+  onClick: () => unknown
+}> = ({ children, additionalStyles, onClick }) => (
+  <div className={cx(styles.modal, additionalStyles)}>
+    <div className={styles.content}>{children}</div>
+    <Overlay onClick={onClick} />
+  </div>
+)
