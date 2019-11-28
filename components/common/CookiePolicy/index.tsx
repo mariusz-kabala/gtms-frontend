b37d52c251df3dd0492606f7ac9a230dfc77cfa9
Mariusz <mariusz@kabala.waw.pl>
2019-11-28 06:24:51 +0100

    feat: fix typings and add tests for cookie policy

--- components/common/CookiePolicy/index.tsx
+++ components/common/CookiePolicy/index.tsx
@@ -1,22 +1,18 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactElement } from 'react'
 import styles from './styles.scss'
 import { getItem, setItem } from 'helpers/localStorage'
 import { Button } from 'components/common/Button'
 
-export const CookiePolicy: FC<{ children: ReactNode }> = ({ children }) => {
+export const CookiePolicy: FC<{ children: ReactElement }> = ({ children }) => {
   const isCookieAccepted = getItem('isCookieAccepted')
 
   return isCookieAccepted === 'true' ? (
     children
   ) : (
-    <div className={styles.cookiePolicy}>
+    <div data-testid="cookie-policy" className={styles.cookiePolicy}>
       <div className={styles.cookie}>
         <p>here goes cookie policy</p>
-        <Button
-          onClick={() => {
-            setItem('isCookieAccepted', 'true')
-          }}
-        >
+        <Button onClick={() => setItem('isCookieAccepted', 'true')}>
           Accept
         </Button>
       </div>
