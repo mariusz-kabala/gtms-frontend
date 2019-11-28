708021807bd125ed40e0d5dd158a55181fbd56c8
rafal <rafal@rafal.comm>
2019-11-28 01:12:38 +0100

    feat: mainWrapper component

---
+++ components/common/WrapperMain/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode } from 'react'
+import styles from './styles.scss'
+import cx from 'classnames'
+
+export const WrapperMain: FC<{
+  additionalStyles: string
+  isActive: boolean
+  children: ReactNode
+}> = ({ additionalStyles, children }) => {
+  // const [state, setState] = useState<boolean>(isActive)
+
+  // <Sidebar
+  //   isActive={state}
+  //   onClose={() => setState(false)}>
+  //   <>
+  //     <img src={Logo} alt='' style={{ width: '100%' }} />
+  //     {/* <Navigation /> */}
+  //   </>
+  // </Sidebar>
+
+  return (
+    <>
+      <div className={cx(styles.wrapper, additionalStyles)}>
+        <div className={styles.wrapperTwo}>{children}</div>
+      </div>
+    </>
+  )
+}
