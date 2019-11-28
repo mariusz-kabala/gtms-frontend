220d7ba2e6dc319fdbe31ffefca7d8ac91af103a
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-11-28 15:30:57 +0100

    chore: add missing error page test cases

--- jest.coverage.ignore.js
+++ jest.coverage.ignore.js
@@ -2,7 +2,6 @@ const PERMANENT_IGNORE = ['/node_modules/', '/dist/', '.d.ts']
 
 const TEMPORARY_IGNORE = [
   './components/common/FlipCard/index.tsx',
-  './pages/_error.tsx',
   './pages/registration/index.tsx',
 ]
 
