8e99658fdf488d86b989928b9d400504b4baeee2
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2019-11-29 21:25:11 +0100

    chore: add tests for api/fetch (#44)

---
+++ api/index.test.ts
@@ -0,0 +1,78 @@
+import { fetchJSON, makeApiUrl } from './index'
+import { FetchMock } from 'jest-fetch-mock'
+
+const fetchMock = fetch as FetchMock
+
+jest.mock('node-fetch', () =>
+  jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: jest.fn(),
+    })
+  )
+)
+
+describe('fetchJSON', () => {
+  beforeEach(() => {
+    ;(fetch as FetchMock).resetMocks()
+  })
+
+  it('Should return proper API url', () => {
+    process.env.FE_API_URL = 'http://fake.com'
+
+    expect(makeApiUrl('test/fake/api')).toBe('http://fake.com/v1/test/fake/api')
+  })
+
+  it('Should make a request with proper params', async done => {
+    fetchMock.mockResponse('{}')
+    await fetchJSON('test/fake/url')
+
+    expect(fetch).toBeCalledTimes(1)
+    expect(fetch).toBeCalledWith('test/fake/url', {
+      timeout: 5000,
+      headers: {},
+    })
+    done()
+  })
+
+  it('Should send proper POST request', async done => {
+    fetchMock.mockResponse('{}')
+
+    await fetchJSON('test/fake/url', {
+      values: {
+        foo: 'bar',
+      },
+    })
+
+    expect(fetch).toBeCalledTimes(1)
+    expect(fetch).toBeCalledWith('test/fake/url', {
+      timeout: 5000,
+      body: '{"foo":"bar"}',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+    done()
+  })
+
+  it('Should add params to request header', async done => {
+    fetchMock.mockResponse('{}')
+    await fetchJSON('test/fake/url', {
+      values: {},
+      headers: {
+        foo: 'bar',
+      },
+    })
+
+    expect(fetch).toBeCalledTimes(1)
+    expect(fetch).toBeCalledWith('test/fake/url', {
+      timeout: 5000,
+      headers: {
+        foo: 'bar',
+      },
+    })
+    done()
+  })
+})
