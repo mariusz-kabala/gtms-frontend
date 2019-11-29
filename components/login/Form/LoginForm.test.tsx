b7c0fb05a77c7f0c7c37869eabe64a21af173e5c
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-11-29 13:59:41 +0100

    chore: add more login form test cases, setup jest-fetch-mock package

--- components/login/Form/LoginForm.test.tsx
+++ components/login/Form/LoginForm.test.tsx
@@ -4,6 +4,9 @@ import { LoginForm } from './index'
 import classNames from './styles.scss'
 import useForm from 'react-hook-form'
 import { ILoginData } from 'api/auth'
+import { FetchMock } from 'jest-fetch-mock'
+
+const fetchMock = fetch as FetchMock
 
 jest.mock('react-hook-form', () => {
   return jest.fn().mockImplementation(() => ({
@@ -57,9 +60,10 @@ describe('<LoginForm />', () => {
     const { getByText } = render(<LoginForm onSuccess={jest.fn()} />)
 
     expect(getByText('form.validation.email.isRequired')).toBeInTheDocument()
+    expect(getByText('form.validation.password.isRequired')).toBeInTheDocument()
   })
 
-  it('Should display errors when clicking on submit button without filling form', () => {
+  it('Should set errors when clicking on submit button without filling form', () => {
     // eslint-disable-next-line
     let onSubmit: any
     const setError = jest.fn()
@@ -83,9 +87,55 @@ describe('<LoginForm />', () => {
 
     act(() => {
       render(<LoginForm onSuccess={jest.fn()} />)
+
       onSubmit({})
     })
 
     expect(setError).toBeCalledTimes(2)
+  })
+
+  it('Should make an request to API to login the user', async done => {
+    fetchMock.mockResponse(
+      JSON.stringify({
+        accessToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3VudHJ5Q29kZSI6IlBMIiwiZW1haWwiOiJtYXJpdXN6QGthYmFsYS53YXcucGwiLCJpZCI6IjVjZGZiNmE2YmFkODhiYjVkYmYxZWNjZiIsImxhbmd1YWdlQ29kZSI6InBsLVBMIiwicm9sZXMiOltdLCJpYXQiOjE1NzUwMzA2OTgsImV4cCI6MTU3NTAzMTU5OH0.L7KVbpbzJSRo6xt1rqyYzdL7RLS_ZrwSV9R_sO5aZuA',
+        refreshToken:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb3VudHJ5Q29kZSI6IlBMIiwiZW1haWwiOiJtYXJpdXN6QGthYmFsYS53YXcucGwiLCJpZCI6IjVjZGZiNmE2YmFkODhiYjVkYmYxZWNjZiIsImxhbmd1YWdlQ29kZSI6InBsLVBMIiwicm9sZXMiOltdLCJpYXQiOjE1NzUwMzA2OTgsImV4cCI6MTU3NTExNzA5OH0.agCvu5lSJwaJL1ralwGKsobDRQjMEWZANrrwwA3gxT0',
+      })
+    )
+    // eslint-disable-next-line
+    let onSubmit: any
+    const onSuccess = jest.fn()
+    ;(useForm as jest.Mock).mockImplementationOnce(() => {
+      return {
+        register: jest.fn(),
+        handleSubmit: (func: (data: ILoginData) => Promise<void>) => {
+          onSubmit = func
+        },
+        errors: {
+          email: {
+            type: 'required',
+          },
+          password: {
+            type: 'required',
+          },
+        },
+        setError: jest.fn(),
+      }
+    })
+
+    act(() => {
+      render(<LoginForm onSuccess={onSuccess} />)
+    })
+
+    await onSubmit({
+      email: 'tester@testing.jest',
+      password: 'loremIpsum',
+    })
+
+    expect(onSuccess).toBeCalledTimes(1)
+    done()
+
+    expect(fetchMock.mock.calls.length).toEqual(1)
   })
 })
