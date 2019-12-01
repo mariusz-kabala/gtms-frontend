f6e2337bf2e6a1df182359b0b43e56cd9bfc883c
Mariusz <mariusz@kabala.waw.pl>
2019-12-01 18:47:01 +0100

    chore: add unit tests for error component

---
+++ components/common/Forms/Error/Error.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { Error } from './index'
+
+describe('<Checkbox />', () => {
+  it('Should be on the page', () => {
+    const { getByTestId, container, getByText } = render(
+      <Error text="Error" additionalStyles={'testingStyles'} />
+    )
+
+    expect(getByTestId('form-error')).toBeInTheDocument()
+    expect(container.querySelector('.testingStyles')).toBeInTheDocument()
+    expect(getByText('Error')).toBeInTheDocument()
+  })
+})
--- components/common/Forms/Error/index.tsx
+++ components/common/Forms/Error/index.tsx
@@ -2,11 +2,11 @@ import React, { FC } from 'react'
 import styles from './styles.scss'
 import cx from 'classnames'
 
-export const Error: FC<{ 
+export const Error: FC<{
   additionalStyles?: string
   text: string
 }> = ({ additionalStyles, text }) => (
-  <span className={cx(styles.error, additionalStyles)}>
-    { text }
+  <span data-testid="form-error" className={cx(styles.error, additionalStyles)}>
+    {text}
   </span>
 )
