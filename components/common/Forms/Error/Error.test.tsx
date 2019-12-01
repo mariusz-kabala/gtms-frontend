0429e78ac3deb1aecc0b5c2fa362f3b6af32c46f
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2019-12-01 19:16:18 +0100

    chore: Add tests for error component (#50)
    * chore: add unit tests for error component
    
    * fix: remove unused import
    
    * fix: use proper test name

---
+++ components/common/Forms/Error/Error.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Error } from './index'
+
+describe('<Error />', () => {
+  it('Should be on the page', () => {
+    const { getByTestId, container, getByText } = render(
+      <Error text="Error" additionalStyles={'testingStyles'} />
+    )
+
+    expect(getByTestId('form-error')).toBeInTheDocument()
+    expect(container.querySelector('.testingStyles')).toBeInTheDocument()
+    expect(getByText('Error')).toBeInTheDocument()
+  })
+})
