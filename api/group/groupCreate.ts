0cf861f7925ff0d9016769800e2ebe5afd7268d2
ra8ga <rafal.furmaga@gmail.com>
2019-12-02 05:57:47 +0100

    feat: introduce group create form (#51)
    * feat: introduce group create form
    
    * update

---
+++ api/group/groupCreate.ts
@@ -0,0 +1,4 @@
+export interface IGroupCreateData {
+  name: string
+  description: string
+}
--- pages/groups/index.tsx
+++ pages/groups/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { GroupCreateForm } from 'components/groups/GroupCreateForm'
 import { NextPage } from 'next'
 import { useCheckAccess } from 'hooks/access'
 import { authOrRedirectToLogin } from 'server/auth'
+import { ImageCover } from 'components/common/ImageCover'
 
 const GroupsPage: NextPage<{
   accessToken?: string
@@ -9,7 +11,43 @@ const GroupsPage: NextPage<{
 }> = ({ accessToken, refreshToken }) => {
   useCheckAccess({ accessToken, refreshToken })
 
-  return <div>Groups page</div>
+  return (
+    <>
+      <h2
+        style={{
+          position: 'relative',
+          zIndex: 99,
+          margin: '50px auto',
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+          color: '#fff',
+          fontSize: '50px',
+        }}
+      >
+        CREATE GROUP
+      </h2>
+      <section
+        style={{
+          // @todo remove it soon
+          position: 'relative',
+          background: 'black',
+          padding: '20px',
+          zIndex: 1,
+        }}
+      >
+        <GroupCreateForm
+        // @todo create on Succes method
+        // onSuccess={() =>
+        //   Router.push({
+        //     pathname: `/${i18n.language}${redirectTo || '/'}`,
+        //   })
+        // }
+        />
+      </section>
+      <ImageCover />
+    </>
+  )
 }
 
 GroupsPage.getInitialProps = authOrRedirectToLogin
