8b39e289ba97bbe5aa1e19bd4a38ed03e79d1f9f
rafal <rafal@rafal.comm>
2019-12-02 10:13:05 +0100

    update

--- pages/andrew/index.tsx
+++ pages/andrew/index.tsx
@@ -6,8 +6,17 @@ import styles from './styles.scss'
 export const AndrewLandingPage: NextPage<IWithTranslations> = ({ t }) => {
   return (
     <div className={styles.wrapper}>
-      <h1>{t('header.header')}</h1>
-      andrew
+      <h1 style={{ display: 'none' }}>{t('header.header')}</h1>
+      <section className={styles.intro}>
+        <h2>Szukamy Andrzeja</h2>
+        <p>Tym razem się uda! Dzięki pomocy internetu odnajdziemy Andrzeja!</p>
+        <p>Stworzymy bazę wszystkich wiosek na Woodstocku.</p>
+      </section>
+      <section className={styles.about}>
+        <h2>Szukamy Andrzeja</h2>
+        <p>Tym razem się uda! Dzięki pomocy internetu odnajdziemy Andrzeja!</p>
+        <p>Stworzymy bazę wszystkich wiosek na Woodstocku.</p>
+      </section>
     </div>
   )
 }
