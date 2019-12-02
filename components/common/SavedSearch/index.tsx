d5a8579d95d7cee17120f6cdc6f9af84c0027609
rafal <rafal@rafal.comm>
2019-12-02 01:37:21 +0100

    add: conditionals

--- components/common/SavedSearch/index.tsx
+++ components/common/SavedSearch/index.tsx
@@ -28,41 +28,51 @@ export const SavedSearch: FC<{
   additionalStyles?: string
 }> = ({ additionalStyles }) => (
   <ul className={additionalStyles}>
-    {mockData.map((search, index) => (
-      <li
-        className={cx(styles.search, {
-          [styles.active]: search.isActive,
-        })}
-        key={index}
-      >
-        <ul className={styles.tags}>
-          {search.tags.map((tag, tagIndex) => {
-            return (
-              <li className={styles.tag} key={tagIndex}>
-                #{tag}
-              </li>
-            )
+    {mockData &&
+      mockData.length > 0 &&
+      mockData.map((search, index) => (
+        <li
+          className={cx(styles.search, {
+            [styles.active]: search.isActive,
           })}
-        </ul>
-        <ul className={styles.locations}>
-          {search.locations.map((location, locationIndex) => {
-            return (
-              <li className={styles.location} key={locationIndex}>
-                {location}
-              </li>
-            )
-          })}
-        </ul>
-        <ul className={styles.groups}>
-          {search.groups.map((group, groupIndex) => {
-            return (
-              <li className={styles.group} key={groupIndex}>
-                {group}
-              </li>
-            )
-          })}
-        </ul>
-      </li>
-    ))}
+          key={index}
+        >
+          {search && search.tags && (
+            <ul className={styles.tags}>
+              {search.tags.map((tag, tagIndex) => {
+                return (
+                  <li className={styles.tag} key={tagIndex}>
+                    #{tag}
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+
+          {search && search.locations && (
+            <ul className={styles.locations}>
+              {search.locations.map((location, locationIndex) => {
+                return (
+                  <li className={styles.location} key={locationIndex}>
+                    {location}
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+
+          {search && search.groups && (
+            <ul className={styles.groups}>
+              {search.groups.map((group, groupIndex) => {
+                return (
+                  <li className={styles.group} key={groupIndex}>
+                    {group}
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </li>
+      ))}
   </ul>
 )
