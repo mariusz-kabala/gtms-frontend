05ef64fd1558bc0aed853fc81460cf782aed49c4
ra8ga <rafal.furmaga@gmail.com>
2019-12-03 08:58:22 +0100

    Geot 18 feat switcher component (#68)
    * GEOT-18: feat: switcher component, GEOT-18
    
    * GEOT-18: cr changes
    
    * Update components/common/Forms/ToggleCheckbox/ToggleCheckbox.test.tsx
    
    Co-Authored-By: Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
    
    * GEOT-18: update tests

---
+++ components/common/Forms/ToggleCheckbox/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react'
+import styles from './styles.scss'
+import { ElementLike } from 'react-hook-form/dist/types'
+
+export const ToggleCheckbox: FC<{
+  additionalStyles?: string
+  checked?: boolean
+  labelChecked?: string
+  labelUnchecked?: string
+  name?: string
+  onChange?: () => unknown
+  reference?: (ref: ElementLike | null) => void
+  lockerIcon?: boolean
+}> = ({
+  additionalStyles,
+  checked = false,
+  labelChecked,
+  labelUnchecked,
+  name,
+  onChange,
+  reference,
+  lockerIcon,
+}) => (
+  <div
+    onClick={onChange}
+    className={additionalStyles}
+    data-testid="toggle-checkbox"
+  >
+    <label className={styles.switch}>
+      <input
+        className={styles.input} /* @todo name it properly */
+        defaultChecked={checked}
+        ref={reference}
+        type="checkbox"
+        name={name}
+        id={name}
+      />
+      <span>
+        {lockerIcon && (
+          <span
+            data-testid="toggle-checkbox-locker-icon"
+            className={styles.locker}
+          />
+        )}
+        <span className={styles.textLabelChecked}>{labelChecked}</span>
+        <span className={styles.textLabelUnchecked}>{labelUnchecked}</span>
+      </span>
+    </label>
+  </div>
+)
