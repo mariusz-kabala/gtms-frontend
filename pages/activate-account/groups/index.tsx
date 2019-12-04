b97c6a79bc0f4c8ee72e56136a2724bcb7b4da20
rafal <rafal@rafal.comm>
2019-12-04 02:33:09 +0100

    GEOT-32 feat: navigation

--- pages/activate-account/groups/index.tsx
+++ pages/activate-account/groups/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { NextPage } from 'next'
-import { SavedSearch } from 'components/common/SavedSearch'
+import { Navigation } from 'components/common/Navigation'
 
 const GroupsPage: NextPage<{}> = () => {
   return (
     <div style={{ width: '340px' }}>
-      <SavedSearch />
+      <Navigation />
     </div>
   )
 }
