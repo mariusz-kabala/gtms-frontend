502b9f1d82331965e992709b8b639ef15ab23331
rafal <rafal@rafal.comm>
2019-12-08 20:23:56 +0100

    GEOT-17: geot-17, create account page

--- components/common/NavigationDot/index.tsx
+++ components/common/NavigationDot/index.tsx
@@ -1,37 +1,34 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC } from 'react'
 import styles from './styles.scss'
 import mockData from './mockData.json'
 
-export const NavigationDot: FC<{ children?: ReactNode }> = ({ children }) => (
-  <>
+export const NavigationDot: FC<{}> = () => (
+  <div className={styles.navigationDot} data-testid="navigationDot">
     <style global jsx>{`
       body {
         padding-bottom: 70px;
       }
     `}</style>
-    <div className={styles.navigationDot}>
-      {children}
-      <ul className={styles.row}>
-        {/* @todo remove key mock, apply normal key */}
-        <li key={23423}>
+    <ul className={styles.row}>
+      {/* @todo remove key mock, apply normal key */}
+      <li key={23423}>
+        <a>
+          <div
+            className={styles.circle}
+            style={{ backgroundImage: `url(${mockData.data[0].img})` }}
+          />
+        </a>
+      </li>
+      {mockData.data.map((value, index) => (
+        <li key={index}>
           <a>
             <div
               className={styles.circle}
-              style={{ backgroundImage: `url(${mockData.data[0].img})` }}
+              style={{ backgroundImage: `url(${value.img})` }}
             />
           </a>
         </li>
-        {mockData.data.map((value, index) => (
-          <li key={index}>
-            <a>
-              <div
-                className={styles.circle}
-                style={{ backgroundImage: `url(${value.img})` }}
-              />
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
+      ))}
+    </ul>
+  </div>
 )
