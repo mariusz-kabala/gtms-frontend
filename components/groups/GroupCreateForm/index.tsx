15e9c33d24aefde025a504faa38519c88da348f2
rafal <rafal@rafal.comm>
2019-12-08 21:58:01 +0100

    GEOT-40: update

--- components/groups/GroupCreateForm/index.tsx
+++ components/groups/GroupCreateForm/index.tsx
@@ -10,25 +10,26 @@ import { Button } from 'components/common/Button'
 
 export const GroupCreateForm: NFC<{}> = () => {
   const { t } = useTranslation('groupCreate')
-  const { register, handleSubmit, errors, setError } = useForm<
+  const { register, handleSubmit, errors } = useForm<
+    // const { register, handleSubmit, errors, setError } = useForm<
     IGroupCreateData
   >()
 
-  const onSubmit = async () => {
-    // @todo create proper validation
-    if (1 > 2) {
-      setError(
-        'passwordConfirmation',
-        'notMatch',
-        t('form.validation.groupName.notMatch')
-      )
-      return
-    }
+  // const onSubmit = async () => {
+  //   // @todo create proper validation
+  //   if (1 > 2) {
+  //     setError(
+  //       'passwordConfirmation',
+  //       'notMatch',
+  //       t('form.validation.groupName.notMatch')
+  //     )
+  //     return
+  //   }
 
-    try {
-      // await registerUserAccount(data) // @todo create groupCreate method
-    } catch (err) {}
-  }
+  //   try {
+  //     // await registerUserAccount(data) // @todo create groupCreate method
+  //   } catch (err) {}
+  // }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
