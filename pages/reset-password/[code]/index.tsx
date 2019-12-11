fd9ea958c171d97c994f7d240ef145d059efcbd0
rafal <rafal@rafal.comm>
2019-12-11 07:37:08 +0100

    update

--- pages/reset-password/[code]/index.tsx
+++ pages/reset-password/[code]/index.tsx
@@ -24,7 +24,7 @@ export const ResetPasswordPage: NextPage<{}> = () => {
             })
             .catch(() => {
                 router.push({
-                    pathname: 'login',
+                    pathname: '/login', 
                 })
             })
     }, [code])
