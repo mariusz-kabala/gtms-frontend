31af104840b178ea0a505e4cae0e81a8f89e0d93
Mariusz <mariusz@kabala.waw.pl>
2019-12-11 20:40:48 +0100

    GEOT-49: test: add tests for useAuth hook and missing test cases for pages

--- __test__/remindPassword.test.tsx
+++ __test__/remindPassword.test.tsx
@@ -3,12 +3,29 @@ import { render, act } from '@testing-library/react'
 import { RemindPasswordPage } from 'pages/remind-password'
 import { RemindPasswordForm } from 'components/remind-password/Form'
 import { useTranslation } from 'i18n'
+import { userStore, IUserStore } from 'state/user'
+import { initAuthSession } from 'helpers/auth'
+import { NextPageContext } from 'next'
+import { redirect } from 'helpers/redirect'
+
+jest.mock('helpers/auth', () => ({
+  initAuthSession: jest.fn().mockImplementation(() => Promise.resolve()),
+}))
+
+jest.mock('helpers/redirect', () => ({
+  redirect: jest.fn(),
+}))
 
 jest.mock('components/remind-password/Form', () => ({
   RemindPasswordForm: jest.fn().mockImplementation(() => <></>),
 }))
 
 describe('<RemindPasswordPage />', () => {
+  beforeEach(() => {
+    ;(initAuthSession as jest.Mock).mockClear()
+    ;(redirect as jest.Mock).mockClear()
+  })
+
   it('Should be rendered', () => {
     const { getByTestId } = render(<RemindPasswordPage />)
 
@@ -55,6 +72,43 @@ describe('<RemindPasswordPage />', () => {
     const props = await RemindPasswordPage.getInitialProps(ctx)
 
     expect(props).toHaveProperty('namespacesRequired')
+    done()
+  })
+
+  it('Should redirect to / if auth session is valid', async done => {
+    if (!RemindPasswordPage.getInitialProps) {
+      return done()
+    }
+
+    const now = new Date().getTime()
+    const update: Partial<IUserStore> = {
+      isInitialized: true,
+      isActive: true,
+      isBlocked: false,
+      session: {
+        accessToken: {
+          expiresAt: now + 100,
+          value: '',
+        },
+        refreshToken: {
+          expiresAt: now + 100,
+          value: '',
+        },
+        createdAt: now,
+      },
+    }
+
+    userStore.update(update)
+
+    const ctx = {} as NextPageContext
+
+    await RemindPasswordPage.getInitialProps(ctx)
+
+    expect(initAuthSession).toBeCalledTimes(1)
+    expect(initAuthSession).toBeCalledWith(ctx)
+    expect(redirect).toBeCalledTimes(1)
+    expect(redirect).toBeCalledWith('/', ctx)
+
     done()
   })
 })
