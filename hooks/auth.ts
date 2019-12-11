5bc6451058abfe78ef7fc20d33a4995865e800c3
Mariusz Kabala <mariusz@kabala.waw.pl>
2019-12-11 17:39:53 +0100

    GEOT-49: refactor: simplify useAuth hook

--- hooks/auth.ts
+++ hooks/auth.ts
@@ -1,22 +1,10 @@
 import { useEffect, useState } from 'react'
-import { init } from 'state/user'
 import { userQuery } from 'state/user'
 
-export function useAuth(
-  accessToken?: string,
-  refreshToken?: string
-): {
+export function useAuth(): {
   isLogged: boolean
-  isInitialized: boolean
 } {
-  if (accessToken && refreshToken) {
-    init({ accessToken, refreshToken })
-  }
-
   const [isLogged, setIsLogged] = useState<boolean>(userQuery.isLogged())
-  const [isInitialized, setIsInitialized] = useState<boolean>(
-    userQuery.isInitialized()
-  )
 
   useEffect(() => {
     const syncLogout = () => (event: { key: string }) => {
@@ -25,19 +13,15 @@ export function useAuth(
       }
     }
 
-    const initSub = userQuery.isInitialized$.subscribe(value =>
-      setIsInitialized(value)
-    )
     const authSub = userQuery.isLogged$.subscribe(value => setIsLogged(!!value))
 
     window.addEventListener('storage', syncLogout)
 
     return () => {
       authSub.unsubscribe()
-      initSub.unsubscribe()
       window.removeEventListener('storage', syncLogout)
     }
   }, [])
 
-  return { isLogged, isInitialized }
+  return { isLogged }
 }
