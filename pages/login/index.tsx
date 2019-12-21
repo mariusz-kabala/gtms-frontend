c95f7d3b18028af945777f717269398f021f8b2e
ra8ga <rafal.furmaga@gmail.com>
2019-12-21 13:47:26 +0100

    GEOT-15: woodstock landing page (#132)
    * andrew page
    
    * update
    
    * update
    
    * update
    
    * update

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { NextPage, NextPageContext } from 'next'
-import { AnimatedComponent } from 'components/common/AnimatedComponent'
 import { Button } from 'components/common/Button'
-import { ImageCover } from 'components/common/ImageCover'
 import { LoginForm } from 'components/login/Form'
-import { Logo } from 'components/common/Logo'
 import { useTranslation, Link } from 'i18n'
 import { parseCookies, destroyCookie } from 'nookies'
 import { SocialButtons } from 'components/login/SocialButtons'
@@ -40,31 +37,42 @@ export const LoginPage: NextPage<{ redirectTo?: string }> = ({
 
   return (
     <div className={styles.wrapper} data-testid="login-page">
-      <div>
-        {error && <div data-testid="login-page-error">{t(error)}</div>}
-        <AnimatedComponent>
-          <Logo />
-        </AnimatedComponent>
-        <LoginForm />
-        <div className={styles.actionButtons}>
-          <Link href="/registration">
-            <Button additionalStyles={styles.btnForgotPassword}>
-              {t('goToRegistration')}
-            </Button>
-          </Link>
-          <Link href="/remind-password">
-            <Button additionalStyles={styles.btnForgotPassword}>
-              {t('goToRemindPassword')}
-            </Button>
-          </Link>
+      <div className={styles.right}>
+        <div>
+          {error && <div data-testid="login-page-error">{t(error)}</div>}
+          <h1>Andrzej can be found</h1>
+          <p>
+            Tempor irure qui excepteur ipsum excepteur qui pariatur deserunt
+            consequat consequat est. Non eiusmod ea non cupidatat occaecat do
+            cupidatat in duis ipsum velit veniam incididunt.
+          </p>
+          <p>
+            Ad amet sunt voluptate consequat aliquip pariatur. Quis laboris
+            incididunt elit.
+          </p>
+          <LoginForm />
+          <div className={styles.actionButtons}>
+            <Link href="/registration">
+              <Button additionalStyles={styles.btnForgotPassword}>
+                {t('goToRegistration')}
+              </Button>
+            </Link>
+            <Link href="/remind-password">
+              <Button additionalStyles={styles.btnForgotPassword}>
+                {t('goToRemindPassword')}
+              </Button>
+            </Link>
+          </div>
+          <SocialButtons
+            additionalStyles={styles.socialButtons}
+            onFailure={() => setError('socialMediaLoginFailed')}
+          />
         </div>
-        <SocialButtons
-          additionalStyles={styles.socialButtons}
-          onFailure={() => setError('socialMediaLoginFailed')}
-        />
-        <div></div>
       </div>
-      <ImageCover />
+      <div
+        className={styles.left}
+        style={{ backgroundImage: `url('/images/temp_images/andrew_bg.jpg')` }}
+      ></div>
     </div>
   )
 }
