d3220b5acfb568122bd2e33138f01e0ceb337318
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-02-27 07:58:18 +0100

    chore: GEOT-74: cleanup

--- packages/app-andrew/src/__test__/AccountPage.tsx
+++ packages/app-andrew/src/__test__/AccountPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { AccountPage } from '../pages/account'
 import { useTranslation } from '@gtms/commons/i18n'
 
