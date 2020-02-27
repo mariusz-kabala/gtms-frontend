1b9c00586ea8990576fc059e0cd7707811194001
ra8ga <rafal.furmaga@gmail.com>
2020-02-27 08:27:19 +0100

    feat: GEOT-74 user account page (#223)
    * feat: GEOT-74: GEOT-74: user account page
    
    * feat: GEOT-74: GEOT-74: user account page
    
    * feat: GEOT-74: GEOT-74: user account page
    
    * feat: GEOT-74: GEOT-74: user account page
    
    * feat: GEOT-74: GEOT-74: user account page
    
    * fix: GEOT-74: GEOT-74-user-account-page fix to pr
    
    * fix: GEOT-74: GEOT-74-user-account-page fix to pr
    
    * fix: GEOT-74: GEOT-74-user-account-page fix to pr
    
    * fix: GEOT-74: GEOT-74-user-account-page fix to pr
    
    * fix: GEOT-74: GEOT-74-user-account-page fix to pr
    
    * fix: GEOT-74: GEOT-74: small amends
    
    * fix: GEOT-74: GEOT-74: small amends
    
    * fix: GEOT-74: GEOT-74: small amends
    
    * fix: GEOT-74: GEOT-74: small amends
    
    * fix: GEOT-74: GEOT-74: small amends
    
    * test: GEOT-74: add missing unit tests
    
    * chore: GEOT-74: cleanup
    
    * fix: GEOT-74: GEOT-74: small amends
    
    Co-authored-by: Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>

--- packages/ui/NavigationDot/index.tsx
+++ packages/ui/NavigationDot/index.tsx
@@ -1,63 +1,50 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC } from 'react'
 import styles from './styles.scss'
+import { Link } from '@gtms/commons/i18n'
 
 /* @todo remove mock */
 const mockData = [
   {
+    id: 0,
+    img: '/images/temp_images/avatar-1.png',
+  },
+  {
+    id: 1,
     img: '/images/temp_images/logo-kreciolatv.png',
   },
   {
+    id: 2,
     img: '/images/temp_images/logo-patrol-1.png',
   },
   {
+    id: 3,
     img: '/images/temp_images/logo-patrol-2.png',
   },
   {
-    img: '/images/temp_images/avatar-1.png',
-  },
-  {
+    id: 4,
     img: '/images/temp_images/logo-kreciolatv.png',
   },
   {
+    id: 5,
     img: '/images/temp_images/logo-patrol-1.png',
   },
   {
-    img: '/images/temp_images/avatar-1.png',
+    id: 6,
+    img: '/images/temp_images/logo-patrol-2.png',
   },
 ]
 
-export const NavigationDot: FC<{ children: ReactNode }> = ({ children }) => (
-  <>
-    <style
-      dangerouslySetInnerHTML={{
-        __html: `
-      body { padding-bottom: 70px }
-    `,
-      }}
-    />
-    <div className={styles.navigationDot}>
-      {children}
-      <ul className={styles.row}>
-        {/* @todo remove key mock, apply normal key */}
-        <li key={23423}>
-          <a>
-            <div
-              className={styles.circle}
-              style={{ backgroundImage: `url(${mockData[0].img})` }}
-            />
-          </a>
-        </li>
-        {mockData.map((value, index) => (
-          <li key={index}>
-            <a>
-              <div
-                className={styles.circle}
-                style={{ backgroundImage: `url(${value.img})` }}
-              />
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
+export const NavigationDot: FC = () => (
+  <ul className={styles.navigationDot} data-testid="navigationDot">
+    {mockData.map(value => (
+      <li key={value.id}>
+        <Link href="/remind-password">
+          <div
+            className={styles.circle}
+            style={{ backgroundImage: `url(${value.img})` }}
+          />
+        </Link>
+      </li>
+    ))}
+  </ul>
 )
