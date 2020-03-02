89f26d8e11eae828f52bf2e084e3587b3668678d
rafal <rafal@rafal.comm>
2020-03-02 05:02:07 +0100

    feat: GEOT-117: GEOT-117: push notifications toggle in user accoun

--- packages/app-andrew/src/components/account/PushNotfications/PushNotifications.test.tsx
+++ packages/app-andrew/src/components/account/PushNotfications/PushNotifications.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { PushNotifications } from './index'
 
 describe('<PushNotifications />', () => {
@@ -7,5 +7,13 @@ describe('<PushNotifications />', () => {
     const { getByTestId } = render(<PushNotifications />)
 
     expect(getByTestId('push-notifications-switcher')).toBeInTheDocument()
+  })
+
+  it('Should change checkbox to active', () => {
+    const { container, getByTestId } = render(<PushNotifications />)
+
+    // fireEvent.click(getByTestId('push-nofitications-switcher'))
+    fireEvent.click(getByTestId('push-notifications-switcher'))
+    expect(container.querySelector('.checked')).toBeInTheDocument()
   })
 })
