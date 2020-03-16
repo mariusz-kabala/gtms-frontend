d3f4198a982ebfce0254389a10980eaafb9ec53b
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-03-16 14:08:16 +0100

    test: GEOT-101: proper file mocking

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '\\.(png|jpg|jpeg)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
   },
   coverageReporters: ['text', 'json', 'html'],
   testURL: 'http://localhost',
