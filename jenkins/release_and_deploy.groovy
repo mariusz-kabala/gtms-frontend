7520048e34d669a17181aaae7a020005d8270ee1
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-03-17 20:43:56 +0100

    ci: master: release pipeline fixes

--- jenkins/release_and_deploy.groovy
+++ jenkins/release_and_deploy.groovy
@@ -11,11 +11,50 @@ pipeline {
     }
 
     stages {
-        stage ('Prepare') {
+        stage ('prepare') {
             steps {
                 script {
-                    sh "printenv"
+                    try {
+                        branch = env.GIT_LOCAL_BRANCH
+                        branch = branch ?: env.GIT_BRANCH
+                        if (branch == 'detached') {
+                            branch = ''
+                        }
+                        branch = branch ?: env.ghprbActualCommit
+                    } catch (e) {
+                        println "GIT BRANCH not detected"
+                    }
+
+                    sh 'git config user.name "jenkins-kabala.tech"'
+                    sh 'git config user.email "jenkins@kabala.tech"'
+
+                    if (!branch) {
+                        error "GIT branch to process not found"
+                    }
+
+                    if (branch.startsWith('origin/')) {
+                        branch = branch.replaceAll('origin/', '')
+                    }
+
+                    println "GIT branch to process: ${branch}"
+                    manager.addShortText(branch, "white", "navy", "1px", "navy")
                 }
+            }
+        }
+
+        stage ('Checkout') {
+            steps {
+                    checkout([
+                            $class                           : 'GitSCM',
+                            branches                         : [[name: "${branch}"]],
+                            browser                          : [$class: 'GithubWeb', repoUrl: "https://github.com/mariusz-kabala/gtms-frontend"],
+                            doGenerateSubmoduleConfigurations: false,
+                            userRemoteConfigs                : [[
+                                credentialsId: 'github',
+                                refspec      : '+refs/pull/*:refs/remotes/origin/pr/*',
+                                url          : "git@github.com:mariusz-kabala/gtms-frontend.git"
+                            ]]
+                    ])
             }
         }
 
