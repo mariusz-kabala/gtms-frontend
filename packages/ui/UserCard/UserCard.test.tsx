3b580a92e04103a573d1b087beef70d8125ff1a3
rafal <rafal@rafal.comm>
2020-03-17 09:05:25 +0100

    feat: GEOT-42: GEOT-42: group main page example

--- packages/ui/UserCard/UserCard.test.tsx
+++ packages/ui/UserCard/UserCard.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { UserCard } from './index'
-import styles from './styles.scss'
 
 describe('<UserCard />', () => {
   it('Should be on the page', () => {
-    const { getByTestId } = render(<UserCard />)
+    const { getByTestId } = render(<UserCard image="/fake/img.png" />)
 
     expect(getByTestId('user-card')).toBeInTheDocument()
   })
