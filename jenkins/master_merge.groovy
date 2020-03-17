afb83d97dfa2254b676b523f8676aa8cc70b803e
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-03-17 18:36:57 +0100

    ci: master: pipeline fix

--- jenkins/master_merge.groovy
+++ jenkins/master_merge.groovy
@@ -57,7 +57,7 @@ pipeline {
             post {
                 always {
                     configFileProvider([configFile(fileId: 'scaleway-s3-config', targetLocation: 'aws-config')]) {
-                        sh "mkdir ~/.aws"
+                        sh "mkdir -p ~/.aws"
                         sh "mv aws-config ~/.aws/config"
                         sh "aws s3 cp coverage s3://unittest/gtmsfrontend/master/ --recursive --acl public-read"
                     }
