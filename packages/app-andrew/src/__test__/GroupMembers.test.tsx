3a500f8d14a13027f80ea4b4df31fe0f1c224e77
rafal <rafal@rafal.comm>
2020-03-21 23:09:08 +0100

    feat: GEOT-128: GEOT-128-group-members-page

--- packages/app-andrew/src/__test__/GroupMembers.test.tsx
+++ packages/app-andrew/src/__test__/GroupMembers.test.tsx
@@ -8,7 +8,23 @@ describe('<GroupMembersPage />', () => {
     const { getByTestId } = render(<GroupMembersPage />)
 
     expect(getByTestId('group-members-page')).toBeInTheDocument()
-
     expect(useTranslation).toBeCalledWith('groupMembers')
+  })
+
+  it('Should return translations namespace from getInitialProps', async done => {
+    if (!GroupMembersPage.getInitialProps) {
+      return done()
+    }
+    // eslint-disable-next-line
+    const ctx: any = null
+
+    const props: {
+      namespacesRequired?: string[]
+    } = await GroupMembersPage.getInitialProps(ctx)
+
+    expect(props).toHaveProperty('namespacesRequired')
+    expect(props.namespacesRequired).toEqual(['groupMembers'])
+
+    done()
   })
 })
