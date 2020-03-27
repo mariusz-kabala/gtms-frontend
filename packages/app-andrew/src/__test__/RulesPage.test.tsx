2efa5dbd9d5a294d44b3de8d62173b83f020d33c
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-03-27 07:36:54 +0100

    test: GEOT-64: increase for now statements threshold

--- packages/app-andrew/src/__test__/RulesPage.test.tsx
+++ packages/app-andrew/src/__test__/RulesPage.test.tsx
@@ -8,4 +8,21 @@ describe('<RulesPage />', () => {
 
     expect(getByTestId('rules-page')).toBeInTheDocument()
   })
+
+  it('Should return translations namespace from getInitialProps', async (done) => {
+    if (!RulesPage.getInitialProps) {
+      return done()
+    }
+    // eslint-disable-next-line
+    const ctx: any = null
+
+    const props: {
+      namespacesRequired?: string[]
+    } = await RulesPage.getInitialProps(ctx)
+
+    expect(props).toHaveProperty('namespacesRequired')
+    expect(props.namespacesRequired).toEqual(['rules'])
+
+    done()
+  })
 })
