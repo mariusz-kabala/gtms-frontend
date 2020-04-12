b8af2d696f3095f40899fba4d248589fda9b7a9d
rafal <rafal@rafal.comm>
2020-04-12 20:28:10 +0200

    feat: GEOT-209: GEOT-209: recently registered users

---
+++ packages/ui/RecentlyRegisteredUsers/index.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useState } from 'react'
+import styles from './styles.scss'
+import cx from 'classnames'
+import { UserAvatar } from '../UserAvatar'
+import { TagGroup } from '../TagGroup'
+import { Tag } from '../Tag'
+
+export const RecentlyRegisteredUsers: FC<{
+  additionalStyles?: string
+}> = ({ additionalStyles }) => {
+  const [state, setState] = useState(false)
+
+  {
+    /* @this component is just a mock */
+  }
+
+  return (
+    <div
+      className={cx(styles.wrapper, additionalStyles)}
+      data-testid="RecentlyRegisteredUsers"
+    >
+      <div className={styles.user}>
+        <UserAvatar
+          onClick={() => setState(!state)}
+          image="https://www.bootdey.com/img/Content/avatar/avatar6.png"
+          additionalStyles={styles.userAvatar}
+        />
+        <span>Marty Mcfly</span>
+      </div>
+      <div className={styles.text}>
+        <p>
+          Ex sint non nisi laborum ex in esse aliquip non veniam. Excepteur
+          irure nisi enim laboris fugiat nostrud consequat do in ea. Et minim
+          pariatur proident esse irure nisi ea non sint qui eu incididunt.
+        </p>
+      </div>
+      <TagGroup>
+        <Tag label="tag" />
+        <Tag label="tag" />
+        <Tag label="tag" />
+        <Tag label="tag" />
+      </TagGroup>
+    </div>
+  )
+}
