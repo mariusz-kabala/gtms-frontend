2114799fd9e164579c05379853ab52386172700a
rafal <rafal@rafal.comm>
2020-04-20 20:53:31 +0200

    feat: GEOT-209: GEOT-209: recently registered users

--- packages/ui/RecentlyRegisteredUsers/index.tsx
+++ packages/ui/RecentlyRegisteredUsers/index.tsx
@@ -1,20 +1,20 @@
 import React, { FC } from 'react'
 import styles from './styles.scss'
 import cx from 'classnames'
-import { UserAvatar } from '../UserAvatar'
+import { useTranslation } from '@gtms/commons/i18n'
+import { UserAvatar } from '../../UserAvatar'
 
 export const RecentlyRegisteredUsers: FC<{
   additionalStyles?: string
 }> = ({ additionalStyles }) => {
-  {
-    /* @this component is just a mock */
-  }
+  const { t } = useTranslation('recentlyRegisteredUsers')
 
   return (
     <div
       className={cx(styles.wrapper, additionalStyles)}
       data-testid="RecentlyRegisteredUsers"
     >
+      <h2 className={styles.output}>{t('header')}</h2>
       <div className={styles.user}>
         <UserAvatar
           image="https://www.bootdey.com/img/Content/avatar/avatar6.png"
