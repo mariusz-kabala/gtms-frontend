4baa676442d6e8deea0a595d31cec3658db3702e
rafal <rafal@rafal.comm>
2020-04-20 18:20:24 +0200

    feat: GEOT-192: GEOT-192: group tags page

--- packages/app-andrew/src/pages/group-tags/index.tsx
+++ packages/app-andrew/src/pages/group-tags/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { NextPage } from 'next'
 import styles from './styles.scss'
 import { useTranslation } from '@gtms/commons/i18n'
-// import { Button } from '@gtms/ui/Button'
 import { TagCard } from '@gtms/ui/TagCard'
 
 export const GroupTagsPage: NextPage<{}> = () => {
@@ -11,7 +10,6 @@ export const GroupTagsPage: NextPage<{}> = () => {
   return (
     <div className={styles.wrapper} data-testid="group-tags-page">
       <h2 className={styles.header}>{t('Tags')}</h2>
-      {/* <Button>Do stuff</Button> */}
       <div className={styles.grid}>
         <TagCard image={'images/temp_images/logo-patrol-2.png'} />
         <TagCard image={'images/temp_images/logo-patrol-2.png'} />
