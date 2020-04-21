8d527659b30ca46f8b83bf07a6cd9f00af18807d
rafal <rafal@rafal.comm>
2020-04-21 08:48:37 +0200

    feat: GEOT-217: GEOT-217-user-card-template

--- packages/app-andrew/src/pages/group-members/index.tsx
+++ packages/app-andrew/src/pages/group-members/index.tsx
@@ -4,80 +4,60 @@ import styles from './styles.scss'
 import { useTranslation } from '@gtms/commons/i18n'
 import { Button } from '@gtms/ui/Button'
 import { UserCard } from '@gtms/ui/UserCard'
+import { UserCardMini } from '@gtms/ui/UserCardMini'
 
 export const GroupMembersPage: NextPage<{}> = () => {
   const { t } = useTranslation('groupMembers')
 
   return (
     <div className={styles.wrapper} data-testid="group-members-page">
-      <h2 className={styles.header}>{t('ostatnio dołączyli')}</h2>
+      <h2 className={styles.header}>{t('header')}</h2>
       <div className={styles.grid}>
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-1.png'} />
-        <UserCard image={'/images/temp_images/logo-sztab-2.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-1.png'} />
-        <UserCard image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCard image={'/images/temp_images/avatar-1.png'} />
+
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-sztab-2.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-1.png'} />
+        <UserCardMini image={'/images/temp_images/logo-patrol-2.png'} />
         <Button additionalStyles={styles.btn}>See more</Button>
       </div>
     </div>
--- packages/ui/RecentlyRegisteredUsers/RecentlyRegisteredUsers.md
+++
@@ -1,7 +0,0 @@
-Basic usage
-
-```
-<div style={{ background: 'black'}}>
-  <RecentlyRegisteredUsers />
-</div>
-```
