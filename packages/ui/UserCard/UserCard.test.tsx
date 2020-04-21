c70a68520b3d8146bb73b873d45a9d50bcde6dce
rafal <rafal@rafal.comm>
2020-04-22 01:44:33 +0200

    feat: GEOT-217: GEOT-217: user card template

--- packages/ui/UserCard/UserCard.test.tsx
+++ packages/ui/UserCard/UserCard.test.tsx
@@ -5,7 +5,7 @@ import { UserCard } from './index'
 describe('<UserCard />', () => {
   it('Should be on the page', () => {
     const { getByTestId } = render(
-      <UserCard onClose={() => unknown} image="/fake/img.png" />
+      <UserCard onClose={jest.fn()} image="/fake/img.png" />
     )
 
     expect(getByTestId('user-card')).toBeInTheDocument()
