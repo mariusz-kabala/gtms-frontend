16648067569877ae4e7b5c9513d150a7ce349ead
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-04-22 11:55:09 +0200

    ci: master: pipeline adjustments

--- jenkins/release_and_build.groovy
+++ jenkins/release_and_build.groovy
@@ -2,7 +2,12 @@ def branch = '';
 def hasNewLock = '0';
 
 pipeline {
-    agent { docker { image 'docker-registry.kabala.tech/node12-with-git:latest' label 'homeSRV2' } }
+    agent { 
+        docker { 
+            image 'docker-registry.kabala.tech/node12-with-git:latest' 
+            label 'homeSRV2' 
+        } 
+    }
     
     environment {
         AWS_ACCESS_KEY_ID = credentials('SCALEWAY_S3_ACCESS_KEY')
