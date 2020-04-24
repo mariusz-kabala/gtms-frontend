042046f2ad9ea5760ecf111ceaf988016d4c0508
rafal <rafal@rafal.comm>
2020-04-24 19:21:39 +0200

    feat: GEOT-219: add file upload

---
+++ packages/ui/UploadFile/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useCallback } from 'react'
+import styles from './styles.scss'
+import cx from 'classnames'
+import {useDropzone} from 'react-dropzone'
+import { IoIosTennisball } from 'react-icons/io'
+
+export const UploadFile: FC<{
+  additionalStyles?: string
+  onClick?: () => unknown
+}> = ({ additionalStyles, onClick }) => {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    // @mariusz?
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div
+      data-testid="user-upload"
+      onClick={onClick}
+      className={cx(styles.wrapper, additionalStyles)}
+    >
+      <div {...getRootProps()} className={styles.dropzone}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <>
+              <IoIosCloudUpload />
+              <p>Drop the files here ...</p>
+            </> :
+            <p>Drag and drop some files here, or click to select files</p>
+        }
+      </div>
+    </div>
+  )
+}
