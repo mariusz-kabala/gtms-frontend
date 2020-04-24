8a70ada1ebdc19b6b0b331fddead8e4c7835c077
rafal <rafal@rafal.comm>
2020-04-24 20:00:20 +0200

    feat: GEOT-219: add file upload

--- packages/ui/UploadFile/UploadFile.test.tsx
+++ packages/ui/UploadFile/UploadFile.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { UploadFile } from './index'
 import styles from './styles.scss'
 
