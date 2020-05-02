65b81f21f498431e3b4c959625fb404b092b6c25
rafal <rafal@rafal.comm>
2020-05-02 20:59:01 +0200

    main-page

--- packages/app-andrew/src/pages/index.tsx
+++ packages/app-andrew/src/pages/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { NextPage } from 'next'
 import styles from './appStyles.scss'
-// import { useAuth } from '@gtms/commons/hooks/auth'
-// import { Link } from '@gtms/commons/i18n'
-// import { useTranslation } from '@gtms/commons/i18n'
-// import { Logout } from '@gtms/ui/Logout'
+import { useAuth } from '@gtms/commons/hooks/auth'
+import { Link } from '@gtms/commons/i18n'
+import { useTranslation } from '@gtms/commons/i18n'
+import { Logout } from '@gtms/ui/Logout'
 import { Button } from '@gtms/ui/Button'
-// import { RecentlyRegisteredUsers } from '@gtms/ui/RecentlyRegisteredUsers'
+import { RecentlyRegisteredUsers } from '@gtms/ui/RecentlyRegisteredUsers'
 
 export const HomePage: NextPage<{}> = () => {
-  // const { t } = useTranslation('homePage')
-  // const { isLogged } = useAuth()
+  const { t } = useTranslation('homePage')
+  const { isLogged } = useAuth()
 
   return (
     <div className={styles.wrapper} data-testid="home-page">
@@ -27,22 +27,18 @@ export const HomePage: NextPage<{}> = () => {
             <Button type="submit" additionalStyles={styles.btn}>
               Add Post
             </Button>
-            <Button type="submit" additionalStyles={styles.btn}>
-              Zaproś znajomych
-            </Button>
           </div>
         </div>
         <div
           className={styles.backgroundImage}
           style={{
-            backgroundImage: `url('/images/temp_images/cover-image.jpg')`,
+            backgroundImage: `url('/images/temp_images/cover-image-girls.jpg')`,
           }}
         />
       </div>
 
-      {/* <h2 className={styles.header}>{t('header')}</h2>
+      <h2 className={styles.header}>{t('header')}</h2>
       <RecentlyRegisteredUsers />
-      <Button additionalStyles={styles.btn}>See more</Button>
       <ul>
         <li>
           <Link href="/group/owsiak">Owsiak</Link>
@@ -54,16 +50,14 @@ export const HomePage: NextPage<{}> = () => {
           <Link href="/group/private-group">Private group</Link>
         </li>
       </ul>
-
-      Welcome to Next.js!
       {isLogged && <p>USER HAS A VALID SESSION!!!</p>}
-      {isLogged && <Logout />} */}
+      {isLogged && <Logout />}
     </div>
   )
 }
 
-// HomePage.getInitialProps = () => {
-//   return Promise.resolve({ namespacesRequired: ['homePage'] })
-// }
+HomePage.getInitialProps = () => {
+  return Promise.resolve({ namespacesRequired: ['homePage'] })
+}
 
 export default HomePage
