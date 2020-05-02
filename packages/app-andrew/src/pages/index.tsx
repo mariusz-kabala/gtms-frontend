254208b7cf25502376e1e11f98e68003df920441
rafal <rafal@rafal.comm>
2020-05-02 17:51:14 +0200

    main-page

--- packages/app-andrew/src/pages/index.tsx
+++ packages/app-andrew/src/pages/index.tsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import { NextPage } from 'next'
-import styles from './styles.scss'
-import { useAuth } from '@gtms/commons/hooks/auth'
-import { Link } from '@gtms/commons/i18n'
-import { useTranslation } from '@gtms/commons/i18n'
-import { Logout } from '@gtms/ui/Logout'
+import styles from './appStyles.scss'
+// import { useAuth } from '@gtms/commons/hooks/auth'
+// import { Link } from '@gtms/commons/i18n'
+// import { useTranslation } from '@gtms/commons/i18n'
+// import { Logout } from '@gtms/ui/Logout'
 import { Button } from '@gtms/ui/Button'
-import { RecentlyRegisteredUsers } from '@gtms/ui/RecentlyRegisteredUsers'
+// import { RecentlyRegisteredUsers } from '@gtms/ui/RecentlyRegisteredUsers'
 
 export const HomePage: NextPage<{}> = () => {
-  const { t } = useTranslation('homePage')
-  const { isLogged } = useAuth()
+  // const { t } = useTranslation('homePage')
+  // const { isLogged } = useAuth()
 
   return (
     <div className={styles.wrapper} data-testid="home-page">
-      <h2 className={styles.header}>{t('header')}</h2>
+      <div className={styles.banner}>
+        <div className={styles.frame}>
+          <div className={styles.desc}>
+            <h2>Dojedź na festiwal z JedziemyNa.pl</h2>
+            <p>
+              Elit excepteur id veniam ea consequat eu excepteur exercitation
+              ullamco nisi sint elit Lorem irure. Exercitation laborum sit
+              proident occaecat dolore pariatur esse tempor fugiat magna
+              incididunt aliquip ullamco.
+            </p>
+            <Button type="submit" additionalStyles={styles.btn}>
+              Add Post
+            </Button>
+            <Button type="submit" additionalStyles={styles.btn}>
+              Zaproś znajomych
+            </Button>
+          </div>
+        </div>
+        <div
+          className={styles.backgroundImage}
+          style={{
+            backgroundImage: `url('/images/temp_images/cover-image.jpg')`,
+          }}
+        />
+      </div>
+
+      {/* <h2 className={styles.header}>{t('header')}</h2>
       <RecentlyRegisteredUsers />
       <Button additionalStyles={styles.btn}>See more</Button>
       <ul>
@@ -31,13 +57,13 @@ export const HomePage: NextPage<{}> = () => {
 
       Welcome to Next.js!
       {isLogged && <p>USER HAS A VALID SESSION!!!</p>}
-      {isLogged && <Logout />}
+      {isLogged && <Logout />} */}
     </div>
   )
 }
 
-HomePage.getInitialProps = () => {
-  return Promise.resolve({ namespacesRequired: ['homePage'] })
-}
+// HomePage.getInitialProps = () => {
+//   return Promise.resolve({ namespacesRequired: ['homePage'] })
+// }
 
 export default HomePage
