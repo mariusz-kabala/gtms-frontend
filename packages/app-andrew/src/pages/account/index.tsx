7fe87b5dabd4de844a3ee7494f94e5e747d989a1
rafal <rafal@rafal.comm>
2020-06-18 01:44:02 +0200

    refactor: GEOT-284: general styling part one

--- packages/app-andrew/src/pages/account/index.tsx
+++ packages/app-andrew/src/pages/account/index.tsx
@@ -182,7 +182,7 @@ export const AccountPage: NextPage<AccountPageProps> = () => {
             additionalStyles={styles.deleteAccount}
             onConfirm={() => null}
           />
-        </>
+        </div>
       )}
     </div>
   )
