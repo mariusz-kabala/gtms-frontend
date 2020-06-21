3fda004ca99ece17a85f094aff33a9092b3abae2
rafal <rafal@rafal.comm>
2020-06-21 17:46:46 +0200

    refactor: GEOT-284: general styling part one

--- packages/ui/NavigationTabs/index.tsx
+++ packages/ui/NavigationTabs/index.tsx
@@ -4,7 +4,7 @@ import cx from 'classnames'
 
 export const NavigationTabs: FC<{
   additionalStyles?: string
-  data?: obj
+  data?: object
 }> = ({
   additionalStyles, data
 }) => {
@@ -12,7 +12,7 @@ export const NavigationTabs: FC<{
     <div className={cx(styles.wrapper, additionalStyles)}>
       <h2 className={styles.header}>{data.header}</h2>
       <ul className={styles.elements}>
-        {data.links.map((item) => <li className={styles.item} key={item.id}>{item.description}</li>)}
+        {data.links.map((item: any) => <li className={styles.item} key={item.id}>{item.description}</li>)}
       </ul>
     </div>
   ) : null
