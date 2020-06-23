c8f93659a0447981b28d7fba94f10865913a9065
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-06-23 11:39:53 +0200

    feat: GEOT-297: autocomplete post tags

--- packages/ui/PostCreate/PostCreate.md
+++ packages/ui/PostCreate/PostCreate.md
@@ -1,5 +1,34 @@
 Basic usage
 
 ```
-  <PostCreate />
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [suggestions, setSuggestions] = React.useState([])
+  const [tags, setTags] = React.useState(['test']);
+  const suggestionsTimeout = React.useRef();
+
+  <PostCreate user={{
+    name: 'John',
+    surname: 'Smith',
+    avatar: {
+      status: 'not-exists',
+    }
+    }}
+    noImage={{
+    '35x35': {
+      jpg: '//via.placeholder.com/35x35'
+    }}}
+    suggestions={suggestions}
+    onLoadSuggestion={(text) => {
+        clearTimeout(suggestionsTimeout.current)
+        setIsLoading(true)
+        suggestionsTimeout.current = setTimeout(() => {
+            setSuggestions(['lorem', 'ipsum', 'dolor', 'sit', 'amet'])
+            setIsLoading(false)
+        }, 2000)
+    }}
+    onLoadSuggestionCancel={() => {
+        clearTimeout(suggestionsTimeout.current)
+        setIsLoading(false)
+    }}
+  />
 ```
--- packages/ui/PostCreate/index.tsx
+++ packages/ui/PostCreate/index.tsx
@@ -1,17 +1,22 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useRef, useEffect, useState } from 'react'
 import styles from './styles.scss'
 import cx from 'classnames'
 import { Button } from '@gtms/ui/Button'
-import { ExpandingTextarea } from '@gtms/ui/Forms/ExpandingTextarea'
 import { useTranslation } from '@gtms/commons/i18n'
 import { FileStatus } from '@gtms/commons/enums'
+import { useDebounce } from '@gtms/commons/hooks/useDebounce'
+import { useExpandingArea } from '@gtms/commons/hooks/expandingArea'
 import { UserAvatar } from '../UserAvatar'
 import { Spinner } from '../Spinner'
 
 export const PostCreate: FC<{
   additionalStyles?: string
   onSubmit: (text: string) => unknown
+  onLoadSuggestion: (text: string) => void
+  onLoadSuggestionCancel: () => void
+  suggestions: string[]
   isLoading?: boolean
+  hintMinLenght?: number
   user: {
     name?: string
     surname?: string
@@ -26,50 +31,93 @@ export const PostCreate: FC<{
     }
   }
   noImage: { [key: string]: { jpg: string; webp?: string } }
-}> = ({ additionalStyles, onSubmit, user, noImage, isLoading = false }) => {
+}> = ({
+  additionalStyles,
+  onSubmit,
+  user,
+  noImage,
+  onLoadSuggestion,
+  suggestions = [],
+  isLoading = false,
+  hintMinLenght = 3,
+}) => {
   const { t } = useTranslation('postCreate')
   const dscRef = useRef<HTMLTextAreaElement | null>(null)
+  const [value, setValue] = useState<string>('')
+  const [query, setQuery] = useState<string>('')
+  const debouncedQuery = useDebounce(query, 500)
+  const { ref, handleInput } = useExpandingArea()
+
+  useEffect(() => {
+    if (debouncedQuery !== '' && debouncedQuery.length >= hintMinLenght) {
+      onLoadSuggestion(debouncedQuery)
+    }
+  }, [debouncedQuery])
 
   return (
     <div
       className={cx(styles.wrapper, additionalStyles)}
       data-testid="postCreate"
     >
-      <div className={styles.text}>
-        {isLoading && <Spinner />}
-        <div className={styles.avatar}>
-          <UserAvatar
-            image={
-              user.avatar.status === FileStatus.ready &&
-              user.avatar.files['35x35']
-                ? user.avatar.files['35x35']
-                : noImage['35x35']
-            }
-            additionalStyles={styles.userAvatar}
-          />
-          <span>{`${user.name || ''} ${user.surname || ''}`.trim()}</span>
-        </div>
-        <ExpandingTextarea
-          reference={dscRef as any}
-          rows={3}
-          placeholder={t('yourMessage')}
-        />
-      </div>
-      <Button
-        onClick={() => {
-          if (!dscRef.current) {
-            return
-          }
-          const dsc = dscRef.current.value
+      <div>
+        <div className={styles.text}>
+          {isLoading && <Spinner />}
+          <div className={styles.avatar}>
+            <UserAvatar
+              image={
+                user.avatar?.status === FileStatus.ready &&
+                user.avatar.files['35x35']
+                  ? user.avatar.files['35x35']
+                  : noImage['35x35']
+              }
+              additionalStyles={styles.userAvatar}
+            />
+            <span>{`${user.name || ''} ${user.surname || ''}`.trim()}</span>
+          </div>
+          <textarea
+            data-testid="form-expanding-textarea"
+            name={name}
+            value={value}
+            onInput={handleInput}
+            onChange={(event) => {
+              setValue(event.target.value)
+              const tags = event.target.value.match(/#(\w+)\b/gi)
+              if (!Array.isArray(tags)) {
+                return
+              }
 
-          dsc && onSubmit(dsc)
-        }}
-        type="submit"
-        disabled={false}
-        additionalStyles={styles.btn}
-      >
-        {t('send')}
-      </Button>
+              setQuery(tags[tags.length - 1].substr(1))
+            }}
+            rows={3}
+            placeholder={t('yourMessage')}
+            ref={ref as any}
+          />
+        </div>
+        <Button
+          onClick={() => {
+            if (!dscRef.current) {
+              return
+            }
+            const dsc = dscRef.current.value
+
+            dsc && onSubmit(dsc)
+          }}
+          type="submit"
+          disabled={false}
+          additionalStyles={styles.btn}
+        >
+          {t('send')}
+        </Button>
+      </div>
+      {suggestions.length > 0 && (
+        <div className={styles.suggestions}>
+          <ul>
+            <li>#woodstock</li>
+            <li>#uran</li>
+            <li>#czeczeni</li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
