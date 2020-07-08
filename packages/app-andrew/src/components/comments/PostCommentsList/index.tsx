8050e33318cb9816aaee31a2c4f63a65f9a473ec
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-07-08 20:58:14 +0200

    feat: GEOT-325: add a comment ui

--- packages/app-andrew/src/components/comments/PostCommentsList/index.tsx
+++ packages/app-andrew/src/components/comments/PostCommentsList/index.tsx
@@ -1,8 +1,18 @@
-import React, { FC } from 'react'
+import React, { FC, useRef, useState, useEffect } from 'react'
+import { findTagsAPI } from '@gtms/api-tags'
 import { PostResponse } from '@gtms/ui/PostSingle/PostResponse'
 import { Spinner } from '@gtms/ui/Spinner'
-import { IComment, IUser } from '@gtms/commons/models'
+import { PostCreate } from '@gtms/ui/PostCreate'
+import { IComment, IUser, IAccountDetails } from '@gtms/commons/models'
+import { SubCommentsList } from '../SubCommentsList'
 import { UserAvatarNoImage } from 'enums'
+import { openLoginModal } from 'state'
+import {
+  IPostCommentsListState,
+  postCommentsListState,
+  postCommentsListState$,
+} from './state.query'
+import styles from './styles.scss'
 
 export const PostCommentsList: FC<{
   isLoading: boolean
@@ -11,7 +21,21 @@ export const PostCommentsList: FC<{
   offset: number
   limit: number
   comments: IComment[]
-}> = ({ isLoading, errorOccured, comments }) => {
+  user: IAccountDetails | null
+}> = ({ isLoading, errorOccured, comments, user }) => {
+  const commentForm = useRef<HTMLDivElement>(null)
+  const [state, setState] = useState<IPostCommentsListState>(
+    postCommentsListState()
+  )
+
+  useEffect(() => {
+    const sub = postCommentsListState$.subscribe((value) => setState(value))
+
+    return () => {
+      sub && !sub.closed && sub.unsubscribe()
+    }
+  }, [])
+
   if (isLoading) {
     return <Spinner />
   }
@@ -31,17 +55,44 @@ export const PostCommentsList: FC<{
 
   return (
     <div>
+      <div className={styles.respondButton}>
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+
+            if (!state.isLogged) {
+              return openLoginModal()
+            }
+
+            if (commentForm.current) {
+              window.scrollTo(0, commentForm.current.offsetTop)
+            }
+          }}
+        >
+          Respond
+        </button>
+      </div>
       {comments.map((comment) => (
-        <PostResponse
-          key={`comment-${comment.id}`}
-          text={comment.text}
-          createdAt={comment.createdAt}
-          owner={comment.owner as IUser}
-          user={null} // todo: fix this later
-          noImage={UserAvatarNoImage}
-        />
-        // todo: pagination is missing
+        <div key={`comment-${comment.id}`}>
+          <PostResponse
+            text={comment.text}
+            createdAt={comment.createdAt}
+            owner={comment.owner as IUser}
+            user={user}
+            noImage={UserAvatarNoImage}
+          />
+          <SubCommentsList user={user} subComments={comment.subComments} />
+        </div>
       ))}
+      <div ref={commentForm}>
+        <PostCreate
+          onSubmit={() => null}
+          fetchTags={findTagsAPI}
+          user={user}
+          noImage={UserAvatarNoImage}
+          onLoginRequest={openLoginModal}
+        />
+      </div>
     </div>
   )
 }
