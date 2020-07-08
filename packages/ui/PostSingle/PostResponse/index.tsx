f956d68bfd524bfa26130bf480608954dae5847d
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-07-08 09:19:13 +0200

    fix: GEOT-325: post creation fixes

--- packages/ui/PostSingle/PostResponse/index.tsx
+++ packages/ui/PostSingle/PostResponse/index.tsx
@@ -8,30 +8,38 @@ import { formatDistance } from 'date-fns'
 import { pl } from 'date-fns/locale'
 import { Link } from '@gtms/commons/i18n'
 import { IImage } from '@gtms/commons/types/image'
+import { IAccountDetails, IUser } from '@gtms/commons/models'
+import { getDisplayName, getImage } from '@gtms/commons/helpers'
 
 export const PostResponse: FC<{
   text: string
   createdAt: string
   additionalStyles?: string
-  owner: string
-  image: IImage
-}> = ({ additionalStyles, text, createdAt, owner, image }) => {
+  owner: IUser
+  noImage: { [key: string]: IImage }
+  user: IAccountDetails | null
+}> = ({ additionalStyles, text, createdAt, owner, noImage, user }) => {
   return (
     <div
       className={cx(styles.wrapper, additionalStyles)}
       data-testid="post-single"
     >
       <div className={styles.header}>
-        <Link href={`/user/${owner}`}>
+        <Link href={`/user/${owner.id}`}>
           <div className={styles.user}>
-            <UserAvatar image={image} additionalStyles={styles.userAvatar} />
-            <span>{owner}</span>
+            <UserAvatar
+              image={getImage('35x35', owner.avatar, noImage)}
+              additionalStyles={styles.userAvatar}
+            />
+            <span>{getDisplayName(owner)}</span>
           </div>
         </Link>
         <span>
           {formatDistance(new Date(createdAt), new Date(), { locale: pl })}
         </span>
-        <DeletePost additionalStyles={styles.deleteBtn} />
+        {user?.id === owner.id && (
+          <DeletePost additionalStyles={styles.deleteBtn} />
+        )}
       </div>
       <div className={styles.desc}>
         <ReactMarkdown className={styles.text} source={text} />
