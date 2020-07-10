a27f1f3effe5d34bf01d594d855562ebc776f539
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-07-10 21:31:02 +0200

    feat: GEOT-333: search post store

---
+++ packages/state-post/src/search/search.store.ts
@@ -0,0 +1,36 @@
+import { EntityState, EntityStore } from '@datorama/akita'
+import { IPost } from '@gtms/commons/models'
+
+export interface IPostsSearchState extends EntityState<IPost, string> {
+  offset: number
+  limit: number
+  total: number
+  query: {
+    tags: string[]
+    groups: string[]
+    users: string[]
+  }
+}
+
+export class PostsSearchStore extends EntityStore<IPostsSearchState> {
+  constructor() {
+    super(
+      {
+        offset: 0,
+        limit: 50,
+        total: 0,
+        query: {
+          tags: [],
+          groups: [],
+          users: [],
+        },
+      },
+      {
+        name: 'postsSearch',
+        resettable: true,
+      }
+    )
+  }
+}
+
+export const postsSearchStore = new PostsSearchStore()
