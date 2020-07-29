8e28e263b260559a951d4731b3cc6c9b4fcdfab9
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2020-07-29 15:48:32 +0200

    fix: GEOT-382: bg chooser - make the ui works (#786)

--- packages/app-andrew/src/components/group-settings/GroupBackground/index.tsx
+++ packages/app-andrew/src/components/group-settings/GroupBackground/index.tsx
@@ -1,15 +1,107 @@
 import React, { FC, useCallback, useState } from 'react'
 import { IGroupBg } from '@gtms/commons/models'
-import { GroupBgNoImage } from '../../../enums'
+import { GroupBgNoImage } from 'enums'
 import { updateGroupAvatar } from '@gtms/state-group'
-// ui
 import { IoIosSettings } from 'react-icons/io'
+// ui
 import { Button } from '@gtms/ui/Button'
 import { FullScreenGallery } from '@gtms/ui/FullScreenGallery'
 import { ImageWithLightbox } from '@gtms/ui/ImageWithLightbox'
 import { Modal } from '@gtms/ui/Modal'
 import { UploadFile } from '@gtms/ui/UploadFile'
+// styles
 import styles from './styles.scss'
+
+const BACKGROUNDS_GALLERY = [
+  {
+    name: 'background1',
+    className: styles.bg1,
+  },
+  {
+    name: 'background2',
+    className: styles.bg2,
+  },
+  {
+    name: 'background3',
+    className: styles.bg3,
+  },
+  {
+    name: 'background4',
+    className: styles.bg4,
+  },
+  {
+    name: 'background5',
+    className: styles.bg5,
+  },
+  {
+    name: 'background6',
+    className: styles.bg6,
+  },
+  {
+    name: 'background7',
+    className: styles.bg7,
+  },
+  {
+    name: 'background8',
+    className: styles.bg8,
+  },
+  {
+    name: 'background9',
+    className: styles.bg9,
+  },
+  {
+    name: 'background10',
+    className: styles.bg10,
+  },
+  {
+    name: 'background11',
+    className: styles.bg11,
+  },
+  {
+    name: 'background1',
+    className: styles.bg1,
+  },
+  {
+    name: 'background2',
+    className: styles.bg2,
+  },
+  {
+    name: 'background3',
+    className: styles.bg3,
+  },
+  {
+    name: 'background4',
+    className: styles.bg4,
+  },
+  {
+    name: 'background5',
+    className: styles.bg5,
+  },
+  {
+    name: 'background6',
+    className: styles.bg6,
+  },
+  {
+    name: 'background7',
+    className: styles.bg7,
+  },
+  {
+    name: 'background8',
+    className: styles.bg8,
+  },
+  {
+    name: 'background9',
+    className: styles.bg9,
+  },
+  {
+    name: 'background10',
+    className: styles.bg10,
+  },
+  {
+    name: 'background11',
+    className: styles.bg11,
+  },
+]
 
 export const GroupBackgroundSettings: FC<{ bg?: IGroupBg }> = ({ bg }) => {
   const [bgUploadStatus, setBgUploadStatus] = useState<{
@@ -54,6 +146,7 @@ export const GroupBackgroundSettings: FC<{ bg?: IGroupBg }> = ({ bg }) => {
       className={styles.wrapper}
     >
       <FullScreenGallery
+        gallery={BACKGROUNDS_GALLERY}
         isActive={isFullScreenGalleryOpen}
         onClose={() => setIsFullScreenGalleryOpen(false)}
       />
