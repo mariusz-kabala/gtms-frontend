c6f1f8bf49668f6cbdb8572d348f2711e032085f
rafal <rafal@rafal.comm>
2020-08-04 14:39:23 +0200

    feat: GEOT-400: GEOT-400: create-your-own-group

--- packages/ui/CreateYourOwnGroup/index.tsx
+++ packages/ui/CreateYourOwnGroup/index.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react'
-import styles from './styles.scss'
 import cx from 'classnames'
 import { Link } from '@gtms/commons/i18n'
 import { useTranslation } from '@gtms/commons/i18n'
+// ui
+import { Picture } from '@gtms/ui/Picture'
+import styles from './styles.scss'
 
 export const CreateYourOwnGroup: FC<{
   additionalStyles?: string
@@ -15,12 +17,13 @@ export const CreateYourOwnGroup: FC<{
       className={cx(styles.wrapper, additionalStyles)}
       data-testid="create-your-own-group"
       onClick={onClick}
-      style={{
-        backgroundImage: `url('/images/white-theme/create-your-own-group.png')`,
-      }}
     >
       <Link href={'/group-create'}>
         <a>
+          <Picture
+            additionalStyles={styles.picture}
+            jpg={'/images/white-theme/create-your-own-group.png'}
+          />
           <h4 className={styles.header}>{t('header')}</h4>
           <p className={styles.desc}>{t('description')}</p>
         </a>
