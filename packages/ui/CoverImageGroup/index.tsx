e4f3b92e0cf5d58553c34bf82dc2169bafd49ec6
rafal <rafal@rafal.comm>
2020-08-13 22:44:14 +0200

    feat: GEOT-423: GEOT-423: choose group cover photo

--- packages/ui/CoverImageGroup/index.tsx
+++ packages/ui/CoverImageGroup/index.tsx
@@ -21,58 +21,80 @@ export const CoverImageGroup: FC<{
       //   backgroundImage: `url('/images/white-theme/spotted-bg-highschool.png')`,
       // }}
     >
-      <div className={cx(styles.step, styles.one)}>
-        <i className={styles.icon}>
-          <BsCardImage />
-        </i>
-        <h2 className={styles.header}>You can add cover photo</h2>
-        <div className={styles.buttons}>
-          <Button
-            additionalStyles={styles.btn}
-            onClick={() => setShowCoverImage(false)}
-          >
-            <i>
-              <IoMdCloseCircle />
-            </i>
-            No, thanks
-          </Button>
-          <Button
-            additionalStyles={styles.btn}
-            onClick={() => setIsStepTwo(true)}
-          >
-            <i>
-              <IoIosAddCircle />
-            </i>
-            Lets add it
-          </Button>
+      <div className={styles.steps}>
+        <div className={cx(styles.step, styles.one)}>
+          <i className={styles.icon}>
+            <BsCardImage />
+          </i>
+          <h2 className={styles.header}>You can add cover photo</h2>
+          <div className={styles.buttons}>
+            <Button
+              additionalStyles={styles.btn}
+              onClick={() => setShowCoverImage(false)}
+            >
+              <i>
+                <IoMdCloseCircle />
+              </i>
+              No, thanks
+            </Button>
+            <Button
+              additionalStyles={styles.btn}
+              onClick={() => setIsStepTwo(true)}
+            >
+              <i>
+                <IoIosAddCircle />
+              </i>
+              Lets add it
+            </Button>
+          </div>
+        </div>
+        <div
+          className={cx(styles.step, styles.two, {
+            [styles.active]: isStepTwo,
+          })}
+        >
+          <div>
+            <Button
+              additionalStyles={styles.btn}
+              onClick={() => setShowCoverImage(false)}
+            >
+              <i>
+                <IoMdCloseCircle />
+              </i>
+              Nah, I do not want cover image
+            </Button>
+            <h2 className={styles.header}>
+              Great!
+              <span className={styles.text}>
+                Page with cover image looks soo much better!
+              </span>
+            </h2>
+          </div>
         </div>
       </div>
       <div
-        className={cx(styles.step, styles.two, {
+        className={cx(styles.choosePhoto, {
           [styles.active]: isStepTwo,
         })}
       >
-        <Button
-          additionalStyles={styles.btn}
-          onClick={() => setShowCoverImage(false)}
-        >
-          <i>
-            <IoMdCloseCircle />
-          </i>
-          Nah, I do not want cover image
-        </Button>
-        <h2 className={styles.header}>
-          Great!
-          <span className={styles.text}>
-            Page with cover image looks soo much better!
-          </span>
-        </h2>
-        <UploadFile
-          additionalStyles={styles.fileUpload}
-          onDrop={() => null}
-          isLoading={false}
-          isError={false}
-        />
+        <ul>
+          <li>
+            <UploadFile
+              additionalStyles={styles.fileUpload}
+              onDrop={() => null}
+              isLoading={false}
+              isError={false}
+            />
+          </li>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
       </div>
     </div>
   )
