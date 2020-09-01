c25c6c66bdaab00b2508fcaf57d531a4c9eaad1f
rafal <rafal@rafal.comm>
2020-09-01 17:36:20 +0200

    feat: GEOT-433: GEOT-433: different small css adjustments

--- packages/ui/FourHundredFour/FourHundredFour.test.tsx
+++ packages/ui/FourHundredFour/FourHundredFour.test.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { FourHundredFour } from '@gtms/ui/FourHundredFour'
-import { useTranslation } from '@gtms/commons/i18n'
 
 describe('<FourHundredFour />', () => {
   it('Should be on the page', () => {
     const { getByTestId } = render(<FourHundredFour />)
 
     expect(getByTestId('four-hundred-four')).toBeInTheDocument()
-    expect(useTranslation).toBeCalledWith('page404')
   })
 
   it('Should have additional css classes', () => {
--- packages/ui/Modal/index.tsx
+++ packages/ui/Modal/index.tsx
@@ -1,11 +1,9 @@
 import React, { FC, ReactNode, useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import styles from './styles.scss'
 import cx from 'classnames'
+import { Overlay } from '@gtms/ui/Overlay'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import useKey from 'use-key-hook'
-// ui
-import { Overlay } from '@gtms/ui/Overlay'
-import styles from './styles.scss'
 
 export const Modal: FC<{
   additionalStyles?: string
@@ -22,18 +20,10 @@ export const Modal: FC<{
     detectKeys: [27],
   })
 
-  const appRoot = document.getElementById('__next')
-
-  return appRoot
-    ? ReactDOM.createPortal(
-        <div
-          className={cx(styles.wrapper, additionalStyles)}
-          data-testid="modal"
-        >
-          <div className={styles.content}>{children}</div>
-          <Overlay onClick={onClose} />
-        </div>,
-        appRoot
-      )
-    : null
+  return (
+    <div className={cx(styles.modal, additionalStyles)} data-testid="modal">
+      <div className={styles.content}>{children}</div>
+      <Overlay onClick={onClose} />
+    </div>
+  )
 }
