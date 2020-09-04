b69723c317a604cb19a5e3cf7c73dc6e224374fe
rafal <rafal@rafal.comm>
2020-09-04 14:20:52 +0200

    feat: GEOT-449: GEOT-449: render modals in portal

--- packages/ui/Modal/index.tsx
+++ packages/ui/Modal/index.tsx
@@ -1,9 +1,11 @@
 import React, { FC, ReactNode, useEffect } from 'react'
-import styles from './styles.scss'
+import { createPortal } from 'react-dom'
 import cx from 'classnames'
-import { Overlay } from '@gtms/ui/Overlay'
+// ui
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import useKey from 'use-key-hook'
+import { Overlay } from '@gtms/ui/Overlay'
+import styles from './styles.scss'
 
 export const Modal: FC<{
   additionalStyles?: string
@@ -20,10 +22,15 @@ export const Modal: FC<{
     detectKeys: [27],
   })
 
-  return (
-    <div className={cx(styles.modal, additionalStyles)} data-testid="modal">
-      <div className={styles.content}>{children}</div>
-      <Overlay onClick={onClose} />
-    </div>
-  )
+  const portal = document.getElementById('__next')
+
+  return portal
+    ? createPortal(
+        <div className={cx(styles.modal, additionalStyles)} data-testid="modal">
+          <div className={styles.content}>{children}</div>
+          <Overlay onClick={onClose} />
+        </div>,
+        portal
+      )
+    : null
 }
