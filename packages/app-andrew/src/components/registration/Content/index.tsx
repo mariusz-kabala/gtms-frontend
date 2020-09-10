9324e1779c3a64eaceb4b37b1c2ce3c307688559
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2020-09-10 11:36:17 +0200

    fix: GEOT-461: display promoted tags on login and registration pa (#854)

---
+++ packages/app-andrew/src/components/registration/Content/index.tsx
@@ -0,0 +1,88 @@
+import React, { FC, useState, useEffect } from 'react'
+import { useTranslation, Link } from '@gtms/commons/i18n'
+import { redirect } from '@gtms/commons/helpers/redirect'
+// state
+import { userQuery } from '@gtms/state-user'
+// components
+import { RegistrationForm } from '../Form'
+import { SocialButtons } from '../../login/SocialButtons'
+// ui
+import { IoMdLogIn } from 'react-icons/io'
+import { Button } from '@gtms/ui/Button'
+import { Picture } from '@gtms/ui/Picture'
+// styles
+import styles from './styles.scss'
+
+export const RegistrationContent: FC<{ loginLink?: string }> = ({
+  children,
+  loginLink = '/login',
+}) => {
+  const { t } = useTranslation('registration')
+  const [error, setError] = useState<string | undefined>()
+
+  useEffect(() => {
+    const sub = userQuery.isActive$.subscribe((isActive) => {
+      if (userQuery.hasData() && !isActive) {
+        redirect('/registration/success')
+      }
+    })
+    return () => {
+      sub && !sub.closed && sub.unsubscribe()
+    }
+  }, [])
+
+  return (
+    <div className={styles.pageWrapper} data-testid="registration-page">
+      <div
+        className={styles.wrapper}
+        style={{
+          backgroundImage: `url('/images/temp-images/login_bg.png')`,
+        }}
+      >
+        <div className={styles.content}>
+          <div>
+            {error && <div data-testid="login-page-error">{t(error)}</div>}
+            <div className={styles.headerWrapper}>
+              <Picture
+                additionalStyles={styles.avatar}
+                jpg={'/images/avatars/avatar-10.png'}
+              />
+              <div>
+                <h2 className={styles.header}>Sign up</h2>
+                <p>
+                  Sunt sint deserunt occaecat reprehenderit est fugiat ex sunt
+                  quis nulla deserunt sit culpa.
+                </p>
+              </div>
+            </div>
+            <SocialButtons
+              additionalStyles={styles.socialButtons}
+              onFailure={() => setError('socialMediaRegistrationFailed')}
+            />
+            <div className={styles.or}>
+              <span>or</span>
+            </div>
+            {error && (
+              <div data-testid="registration-page-error">{t(error)}</div>
+            )}
+            <RegistrationForm
+              additionalStyles={styles.form}
+              onError={() => setError('registrationFailed')}
+            />
+            <div className={styles.actionButtons}>
+              <Link href={loginLink}>
+                <Button additionalStyles={styles.btn}>
+                  <i>
+                    <IoMdLogIn />
+                  </i>
+                  {t('goToLogin')}
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
