ea9df8b768ba2c445b9983aafde193b8654b0dd0
rafal <rafal@rafal.comm>
2020-11-28 15:01:29 +0100

    fix: GEOT-676: GEOT-676: not show promoted tags if no tag added a

--- packages/ui/PromotedTags/index.tsx
+++ packages/ui/PromotedTags/index.tsx
@@ -1,14 +1,21 @@
-import React, { FC, useCallback } from 'react'
+import React, { FC, useCallback, useState } from 'react'
 import cx from 'classnames'
 // commons
 import { FileStatus } from '@gtms/commons/enums'
 import { IPromotedTag } from '@gtms/commons/models'
 import { IImage } from '@gtms/commons/types/image'
 // ui
-import { IoIosAddCircle, IoMdTrash, IoIosSettings } from 'react-icons/io'
-import { Button } from '../Button'
-import { Spinner } from '../Spinner'
+import {
+  IoIosAddCircle,
+  IoIosCloseCircle,
+  IoIosCheckbox,
+  IoMdTrash,
+  IoIosSettings,
+} from 'react-icons/io'
+import { Button } from '@gtms/ui/Button'
+import { Modal } from '@gtms/ui/Modal'
 import { Picture } from '../Picture'
+import { Spinner } from '../Spinner'
 import { UploadedPicture } from '../UploadedPicture'
 // style
 import styles from './styles.scss'
@@ -39,6 +46,7 @@ export const PromotedTags: FC<{
   isAdmin = false,
   activeTags = [],
 }) => {
+  const [TagToDelete, setTagToDelete] = useState<IPromotedTag | null>(null)
   const onClick = useCallback(() => {
     onNoRecordsClick && onNoRecordsClick()
   }, [onNoRecordsClick])
@@ -46,12 +54,47 @@ export const PromotedTags: FC<{
   return (
     <div className={additionalStyles} data-testid="promoted-tags">
       {isLoading && <Spinner />}
-
+      {TagToDelete && (
+        <Modal
+          additionalStyles={styles.deleteTagModal}
+          onClose={() => setTagToDelete(null)}
+        >
+          <h2 className={styles.header}>Are you sure you want delete tag?</h2>
+          <p className={styles.desc}>
+            Eteu in occaecat occaecat consectetur et laboris aliquip.
+          </p>
+          <div className={styles.buttons}>
+            <Button
+              additionalStyles={styles.no}
+              testid="delete-account-cancel"
+              onClick={() => setTagToDelete(null)}
+            >
+              <i>
+                <IoIosCloseCircle />
+              </i>
+              No
+            </Button>
+            <Button
+              testid="delete-account-confirm"
+              additionalStyles={styles.yes}
+              onClick={() => {
+                onDeleteRecordClick && onDeleteRecordClick(TagToDelete)
+                setTagToDelete(null)
+              }}
+            >
+              <i>
+                <IoIosCheckbox />
+              </i>
+              Yes
+            </Button>
+          </div>
+        </Modal>
+      )}
       <div className={cx(styles.items)}>
         {tags.length > 0 &&
           tags.map((tag) => (
             <div
-              onClick={() => onTagClick && onTagClick(tag)}
+              // onClick={() => onTagClick && onTagClick(tag)}
               className={cx(styles.item, styles.typeTwo, {
                 [styles.active]: activeTags.includes(tag.tag),
               })}
@@ -98,11 +141,7 @@ export const PromotedTags: FC<{
                         <IoIosSettings />
                       </i>
                     </li>
-                    <li
-                      onClick={() =>
-                        onDeleteRecordClick && onDeleteRecordClick(tag)
-                      }
-                    >
+                    <li onClick={() => setTagToDelete(tag)}>
                       <i>
                         <IoMdTrash />
                       </i>
