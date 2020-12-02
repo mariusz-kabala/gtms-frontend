8a4fabb15c6a1ca23e5fa79a8394a2932edc69a5
ra8ga <rafal.furmaga@gmail.com>
2020-12-02 14:54:03 +0100

    feat: GEOT-676: not show promoted tags if no tag added (#910)
    * fix: GEOT-673: GEOT-673: adjust css to latest desings
    
    * fix: GEOT-673: GEOT-673: adjust css to latest desings
    
    * fix: GEOT-675: GEOT-675: rename to GroupSettingsSidebar
    
    * fix: GEOT-675: GEOT-675: move sidebar settings to components/grou
    
    * fix: GEOT-675: GEOT-675: cleanup
    
    * fix: GEOT-675: GEOT-675: update
    
    * fix: GEOT-675: GEOT-675: update
    
    * fix: GEOT-675: GEOT-675: update
    
    * fix: GEOT-676: GEOT-676: not show promoted tags if no tag added a
    
    * fix: GEOT-676: GEOT-676: not show promoted tags if no tag added a
    
    Co-authored-by: rafal <rafal@rafal.comm>

--- packages/ui/PromotedTags/index.tsx
+++ packages/ui/PromotedTags/index.tsx
@@ -1,18 +1,25 @@
-import React, { FC, useCallback } from 'react'
+import React, { FC, useCallback, useState } from 'react'
 import cx from 'classnames'
 // commons
 import { FileStatus } from '@gtms/commons/enums'
 import { IPromotedTag } from '@gtms/commons/models'
 import { IImage } from '@gtms/commons/types/image'
 // ui
-import { IoIosAddCircle, IoMdTrash, IoIosSettings } from 'react-icons/io'
-import { Button } from '../Button'
-import { Spinner } from '../Spinner'
+import {
+  IoIosAddCircle,
+  IoIosCloseCircle,
+  IoIosCheckbox,
+  IoMdTrash,
+  IoIosSettings,
+} from 'react-icons/io'
+import { Button } from '@gtms/ui/Button'
+import { Modal } from '@gtms/ui/Modal'
 import { Picture } from '../Picture'
+import { Spinner } from '../Spinner'
 import { UploadedPicture } from '../UploadedPicture'
 // style
 import styles from './styles.scss'
-import { getImage } from '@gtms/commons'
+// import { getImage } from '@gtms/commons'
 
 export const PromotedTags: FC<{
   additionalStyles?: string
@@ -39,6 +46,7 @@ export const PromotedTags: FC<{
   isAdmin = false,
   activeTags = [],
 }) => {
+  const [TagToDelete, setTagToDelete] = useState<IPromotedTag | null>(null)
   const onClick = useCallback(() => {
     onNoRecordsClick && onNoRecordsClick()
   }, [onNoRecordsClick])
@@ -46,46 +54,55 @@ export const PromotedTags: FC<{
   return (
     <div className={additionalStyles} data-testid="promoted-tags">
       {isLoading && <Spinner />}
-
+      {TagToDelete && (
+        <Modal
+          additionalStyles={styles.deleteTagModal}
+          onClose={() => setTagToDelete(null)}
+        >
+          <h2 className={styles.header}>Are you sure you want delete tag?</h2>
+          <p className={styles.desc}>
+            Eteu in occaecat occaecat consectetur et laboris aliquip.
+          </p>
+          <div className={styles.buttons}>
+            <Button
+              additionalStyles={styles.no}
+              testid="delete-account-cancel"
+              onClick={() => setTagToDelete(null)}
+            >
+              <i>
+                <IoIosCloseCircle />
+              </i>
+              No
+            </Button>
+            <Button
+              testid="delete-account-confirm"
+              additionalStyles={styles.yes}
+              onClick={() => {
+                onDeleteRecordClick && onDeleteRecordClick(TagToDelete)
+                setTagToDelete(null)
+              }}
+            >
+              <i>
+                <IoIosCheckbox />
+              </i>
+              Yes
+            </Button>
+          </div>
+        </Modal>
+      )}
       <div className={cx(styles.items)}>
         {tags.length > 0 &&
           tags.map((tag) => (
             <div
-              onClick={() => onTagClick && onTagClick(tag)}
-              className={cx(styles.item, styles.typeTwo, {
+              className={cx(styles.item, {
                 [styles.active]: activeTags.includes(tag.tag),
               })}
               key={`promoted-${tag.id}`}
-              style={
-                tag.logo.status === FileStatus.ready
-                  ? {
-                      backgroundImage: `url(${
-                        getImage('200x200', tag.logo).jpg
-                      }`,
-                    }
-                  : {}
-              }
             >
               {!tag.logo.status && <Picture {...noImage['200x200']} />}
               {tag.logo.status && tag.logo.status !== FileStatus.ready && (
                 <UploadedPicture jpg={tag.logo.files[0]} />
               )}
-              <div
-                className={styles.image}
-                style={
-                  tag.logo.status === FileStatus.ready
-                    ? {
-                        backgroundImage: `url(${
-                          getImage('200x200', tag.logo).jpg
-                        }`,
-                      }
-                    : {}
-                }
-              />
-              <div className={styles.desc}>
-                <h3>#{tag.tag}</h3>
-                <p>{tag.description}</p>
-              </div>
               {isAdmin && (
                 <div className={styles.adminPanel}>
                   <ul>
@@ -98,11 +115,7 @@ export const PromotedTags: FC<{
                         <IoIosSettings />
                       </i>
                     </li>
-                    <li
-                      onClick={() =>
-                        onDeleteRecordClick && onDeleteRecordClick(tag)
-                      }
-                    >
+                    <li onClick={() => setTagToDelete(tag)}>
                       <i>
                         <IoMdTrash />
                       </i>
@@ -110,6 +123,16 @@ export const PromotedTags: FC<{
                   </ul>
                 </div>
               )}
+              <div
+                className={styles.content}
+                // style={tag.logo.status === FileStatus.ready ? { backgroundImage: `url(${getImage('200x200', tag.logo).jpg}`} : {}}
+                onClick={() => onTagClick && onTagClick(tag)}
+              >
+                <div className={styles.desc}>
+                  <h3>#{tag.tag}</h3>
+                  <p>{tag.description}</p>
+                </div>
+              </div>
             </div>
           ))}
 
@@ -122,7 +145,7 @@ export const PromotedTags: FC<{
           </button>
         )}
       </div>
-      <Button additionalStyles={styles.btn}>
+      <Button additionalStyles={styles.btnShowMore}>
         <Spinner size="sm" />
         Show more...
       </Button>
