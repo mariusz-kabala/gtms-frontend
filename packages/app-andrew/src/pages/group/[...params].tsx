d35dcb631fb4f5dc354ad259d4bf67c0dd08d33f
rafal <rafal@rafal.comm>
2020-12-03 10:55:21 +0100

    feat: GEOT-687: GEOT-687: better styles for post details

--- packages/app-andrew/src/pages/group/[...params].tsx
+++ packages/app-andrew/src/pages/group/[...params].tsx
@@ -65,8 +65,6 @@ import { SearchBar } from '@gtms/ui/SearchBar'
 import { Spinner } from '@gtms/ui/Spinner'
 // styles
 import styles from './styles.scss'
-// mock data
-import { mockTags } from './mock'
 
 type GroupPageProps = {
   namespacesRequired: readonly string[]
@@ -259,6 +257,81 @@ const GroupPage: NextPage<GroupPageProps> = (props) => {
       sub && !sub.closed && sub.unsubscribe()
     }
   }, [])
+
+  const mockTags = [
+    {
+      id: 1,
+      name: '#polandrock',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-1.png',
+    },
+    {
+      id: 2,
+      name: '#wacken',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-2.png',
+    },
+    {
+      id: 3,
+      name: '#tinthepark',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-3.png',
+    },
+    {
+      id: 4,
+      name: '#openair',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-4.png',
+    },
+    {
+      id: 5,
+      name: '#szieget',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-5.png',
+    },
+    {
+      id: 6,
+      name: '#rockampark',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-6.png',
+    },
+    {
+      id: 7,
+      name: '#RoskildeFestival',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-7.png',
+    },
+    {
+      id: 8,
+      name: '#burningManFestival',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-8.png',
+    },
+    {
+      id: 9,
+      name: '#szieget',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-9.png',
+    },
+    {
+      id: 10,
+      name: '#rockampark',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-10.png',
+    },
+    {
+      id: 11,
+      name: '#RoskildeFestival',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-11.png',
+    },
+    {
+      id: 12,
+      name: '#burningManFestival',
+      desc: 'Velit fugiat quis laboris ut nostrud adipisiadipisicing.',
+      image: 'avatar-3.png',
+    },
+  ]
 
   return (
     <div className={styles.pageWrapper}>
