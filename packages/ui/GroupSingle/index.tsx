b8740f7fa6425697f44ad84df99a82d81416fa67
rafal <rafal@rafal.comm>
2020-12-06 22:25:45 +0100

    feat: GEOT-694: GEOT-694: style search page

--- packages/ui/GroupSingle/index.tsx
+++ packages/ui/GroupSingle/index.tsx
@@ -34,60 +34,49 @@ export const GroupSingle: FC<{
 
   return (
     <div data-testid="group-single" className={styles.wrapper}>
-      <div className={styles.content}>
-        <div className={styles.partOne}>
-          <Link href={`/group/${slug}`}>
-            <a>
-              <Picture
-                additionalStyles={styles.avatar}
-                {...getImage('200x200', avatar, noUserAvatar)}
-              />
-            </a>
-          </Link>
-          <div className={styles.nameDesc}>
-            <div className={styles.desc}>
-              <Link href={`/group/${slug}`}>
-                <h2 className={styles.header}>{name}</h2>
-              </Link>
-              {description && <p className={styles.desc}>{description}</p>}
-            </div>
+      <Link href={`/group/${slug}`}>
+        <a className={styles.avatarNameDesc}>
+          <Picture
+            additionalStyles={styles.avatar}
+            {...getImage('200x200', avatar, noUserAvatar)}
+          />
+          <div>
+            {' '}
+            {/* for aliging vertically */}
+            <h2 className={styles.header}>{name}</h2>
+            {description && <p className={styles.desc}>{description}</p>}
           </div>
-          <div className={cx(styles.users)}>
-            <h3 className={styles.header}>{t('groupsMembers')}</h3>
-            {Array.isArray(members) && members.length > 0 && (
-              <ul
-                className={styles.items}
-                data-testid="recently-registered-users"
-              >
-                {members.map((member) => (
-                  <li className={styles.user} key={`member-${member.id}`}>
-                    <UserAvatar
-                      image={getImage('50x50', member.avatar, noUserAvatar)}
-                      additionalStyles={styles.userAvatar}
-                    />
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        </div>
-        <div className={styles.partTwo}>
-          <div className={styles.tags}>
-            {Array.isArray(tags) && tags.length > 0 ? (
-              tags.map((tag) => (
-                <Tag
-                  additionalStyles={cx({
-                    [styles.activeTag]: activeTags.includes(tag),
-                  })}
-                  key={`tag-${tag}`}
-                  label={tag}
+        </a>
+      </Link>
+      {Array.isArray(members) && members.length > 0 && (
+        <div className={cx(styles.users)}>
+          <h3 className={styles.header}>{t('groupsMembers')}</h3>
+          <ul className={styles.items} data-testid="recently-registered-users">
+            {members.map((member) => (
+              <li className={styles.user} key={`member-${member.id}`}>
+                <UserAvatar
+                  image={getImage('50x50', member.avatar, noUserAvatar)}
+                  additionalStyles={styles.userAvatar}
                 />
-              ))
-            ) : (
-              <p>{t('tags-not-added-yet')}</p>
-            )}
-          </div>
+              </li>
+            ))}
+          </ul>
         </div>
+      )}
+      <div className={styles.tags}>
+        {Array.isArray(tags) && tags.length > 0 ? (
+          tags.map((tag) => (
+            <Tag
+              additionalStyles={cx(styles.tag, {
+                [styles.activeTag]: activeTags.includes(tag),
+              })}
+              key={`tag-${tag}`}
+              label={tag}
+            />
+          ))
+        ) : (
+          <p>{t('tags-not-added-yet')}</p>
+        )}
       </div>
     </div>
   )
