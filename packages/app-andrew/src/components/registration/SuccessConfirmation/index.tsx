8f19672d3eb3b00aa502dc98cc229923c79b8522
rafal <rafal@rafal.comm>
2020-12-15 23:33:44 +0100

    feat: GEOT-713: GEOT-713: spinner inside buttons and other small c

--- packages/app-andrew/src/components/registration/SuccessConfirmation/index.tsx
+++ packages/app-andrew/src/components/registration/SuccessConfirmation/index.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from 'react'
 import { useTranslation } from '@gtms/commons/i18n'
 import { Link } from '@gtms/commons/i18n'
+import styles from './styles.scss'
 
 export const SuccessConfirmation: FC<{}> = () => {
   const { t } = useTranslation('registration')
 
   return (
-    <div data-testid="registration-success-confirmation">
-      <h2>{t('registrationSuccessMessage')}</h2>
-      <div>
+    <>
+      <div
+        className={styles.wrapper}
+        data-testid="registration-success-confirmation"
+      >
+        <h2>{t('registrationSuccessMessage')}</h2>
         <Link href={`login`}>{t('goToLoginPage')}</Link>
       </div>
-    </div>
+      <div
+        className={styles.pageBg}
+        style={{ backgroundImage: `url('/images/temp-images/login-bg-2.png')` }}
+      />
+    </>
   )
 }
