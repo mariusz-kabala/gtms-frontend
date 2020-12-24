1eb67955bd90a4f8986e152368f93b6cec438429
rafal <rafal@rafal.comm>
2020-12-24 20:15:32 +0100

    fix: GEOT-735: GEOT-735: restyle notifications

--- packages/app-andrew/src/components/commons/NotificationsSidebar/index.tsx
+++ packages/app-andrew/src/components/commons/NotificationsSidebar/index.tsx
@@ -99,7 +99,6 @@ export const NotificationsSidebar: FC<{
                   return (
                     <NotificationAPI
                       {...(notification.data as INotification)}
-                      additionalStyles={styles.notification}
                       key={`notification-${notification.id}`}
                     />
                   )
