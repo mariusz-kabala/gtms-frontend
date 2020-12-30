7c72100c401db1857e974f153bca959d7249243c
ra8ga <rafal.furmaga@gmail.com>
2020-12-30 17:05:16 +0100

    fix: GEOT-756: login reg remind reset password styles (#968)
    * fix: GEOT-756: GEOT-756-login-reg-remind-reset-password-styles
    
    * fix: GEOT-756: GEOT-756-login-reg-remind-reset-password-styles
    
    * fix: GEOT-756: GEOT-756-login-reg-remind-reset-password-styles
    
    * fix: GEOT-756: GEOT-756-login-reg-remind-reset-password-styles
    
    * Update index.tsx
    
    Co-authored-by: rafal <rafal@rafal.comm>
    Co-authored-by: Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>

--- packages/app-andrew/src/pages/reset-password/[code]/index.tsx
+++ packages/app-andrew/src/pages/reset-password/[code]/index.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { NextPage, NextPageContext } from 'next'
 import { useRouter } from 'next/router'
 import { ResetPasswordForm } from '../../../components/reset-password/Form'
-import { Spinner } from '@gtms/ui/Spinner'
 import { useTranslation, Link } from '@gtms/commons/i18n'
 import { checkCodeReq } from '@gtms/api-auth'
 import { redirect } from '@gtms/commons/helpers/redirect'
 import { hasAuthSessionCookies } from '@gtms/state-user'
+import { IoMdCheckmark } from 'react-icons/io'
+// ui
 import { Button } from '@gtms/ui/Button'
+import { Spinner } from '@gtms/ui/Spinner'
 import styles from './styles.scss'
 
 export const ResetPasswordPage: NextPage<{}> = () => {
@@ -30,35 +32,46 @@ export const ResetPasswordPage: NextPage<{}> = () => {
   }, [code])
 
   return (
-    <div className={styles.pageWrapper} data-testid="remind-password-page">
-      <div className={styles.wrapper}>
-        <div>
-          <h2 className={styles.header}>
-            {/* @todo GEOT-109 - put proper translations everywhere */}
-            {t('Podaj nowe haslo do swojego konta')}
-          </h2>
-          {isLoading && <Spinner />}
-          {!isLoading && !isPasswordChanged && (
-            <ResetPasswordForm
-              code={code}
-              onSuccess={() => setIsPasswordChanged(true)}
-            />
-          )}
-          {!isLoading && isPasswordChanged && (
-            <div>
-              <p data-testid="reset-password-changed-confirmation">
-                {t('passwordHasBeenChanged')}
-              </p>
-              <p>
+    <>
+      <div className={styles.pageWrapper} data-testid="remind-password-page">
+        <div className={styles.wrapper}>
+          <div className={styles.centerIt}>
+            {isLoading && <Spinner additionalStyles={styles.spinner} />}
+            {!isLoading && !isPasswordChanged && (
+              <>
+                <h3 className={styles.header}>
+                  {/* @todo GEOT-109 - put proper translations everywhere */}
+                  {t('Podaj nowe haslo do swojego konta')}
+                </h3>
+                <ResetPasswordForm
+                  code={code}
+                  onSuccess={() => setIsPasswordChanged(true)}
+                />
+              </>
+            )}
+            {!isLoading && isPasswordChanged && (
+              <>
+                <h3
+                  className={styles.header}
+                  data-testid="reset-password-changed-confirmation"
+                >
+                  <i>
+                    <IoMdCheckmark />
+                  </i>
+                  {t('passwordHasBeenChanged')}
+                </h3>
                 <Link href="/login">
-                  <Button>{t('goToLogin')}</Button>
+                  <Button additionalStyles={styles.btn}>
+                    {t('goToLogin')}
+                  </Button>
                 </Link>
-              </p>
-            </div>
-          )}
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.pageBg} />
+    </>
   )
 }
 
