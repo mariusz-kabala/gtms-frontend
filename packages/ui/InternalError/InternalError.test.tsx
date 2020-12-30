3bd0809810725854cce812bcec9ed5c72134bd6e
rafal <rafal@rafal.comm>
2020-12-30 12:01:44 +0100

    fix: GEOT-755: GEOT-755: internal error component

---
+++ packages/ui/InternalError/InternalError.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { InternalError } from 'packages/ui/InternalError'
+
+describe('<InternalError />', () => {
+  it('Should be on the page', () => {
+    const { getByTestId } = render(<InternalError />)
+
+    expect(getByTestId('internal-error')).toBeInTheDocument()
+  })
+
+  it('Should have additional css classes', () => {
+    const { container } = render(<InternalError additionalStyles={'cssTest'} />)
+
+    expect(container.querySelector('.cssTest')).toBeInTheDocument()
+  })
+})
