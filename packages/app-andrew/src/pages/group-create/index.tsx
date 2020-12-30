5b7419144ca60a741c74eabf2d42c2e49a0ed721
ra8ga <rafal.furmaga@gmail.com>
2020-12-30 06:34:15 +0100

    feat: GEOT-752: better group create (#964)
    * fix: GEOT-750: GEOT-750: unify font size font weight mixins
    
    * fix: GEOT-752: GEOT-752: better group create
    
    * fix: GEOT-752: GEOT-752: better group create
    
    Co-authored-by: rafal <rafal@rafal.comm>

--- packages/app-andrew/src/pages/group-create/index.tsx
+++ packages/app-andrew/src/pages/group-create/index.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
+import cx from 'classnames'
 import { NextPage, NextPageContext } from 'next'
-import styles from './styles.scss'
 import { useTranslation } from '@gtms/commons/i18n'
 import { GroupCreate } from '../../components/group/GroupCreate'
-import { Picture } from '@gtms/ui/Picture'
 import { hasAuthSessionCookies } from '@gtms/state-user'
 import { redirect } from '@gtms/commons/helpers/redirect'
+// styles
+import { Picture } from '@gtms/ui/Picture'
+import styles from './styles.scss'
 
 export const GroupCreatePage: NextPage<{}> = () => {
   const { t } = useTranslation('groupCreate')
 
   return (
-    <div className={styles.pageWrapper}>
-      <div className={styles.wrapper} data-testid="group-create-page">
-        <div>
-          <Picture
-            additionalStyles={styles.image}
-            jpg={'/images/white-theme/group-create.png'}
-          />
-          <h2 className={styles.header}>{t('header')}</h2>
-          <p className={styles.desc}>{t('subheader')}</p>
-          <GroupCreate />
-        </div>
+    <div className={styles.pageWrapper} data-testid="group-create-page">
+      <div className={styles.form}>
+        <Picture
+          additionalStyles={styles.image}
+          jpg={'/images/white-theme/group-create.png'}
+        />
+        <h2 className={styles.header}>{t('header')}</h2>
+        <p className={styles.desc}>{t('subheader')}</p>
+        <GroupCreate />
       </div>
+      <ul className={styles.images}>
+        <li className={cx(styles.image, styles.one)} />
+        <li className={cx(styles.image, styles.two)} />
+        <li className={cx(styles.image, styles.three)} />
+        <li className={cx(styles.image, styles.four)} />
+        <li className={cx(styles.image, styles.five)} />
+        <li className={cx(styles.image, styles.six)} />
+        <li className={cx(styles.image, styles.seven)} />
+        <li className={cx(styles.image, styles.eight)} />
+        <li className={cx(styles.image, styles.nine)} />
+        <li className={cx(styles.image, styles.ten)} />
+        <li className={cx(styles.image, styles.eleven)} />
+        <li className={cx(styles.image, styles.twelve)} />
+      </ul>
     </div>
   )
 }
