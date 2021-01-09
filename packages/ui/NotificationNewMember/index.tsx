e5742124d21dbcf3703e9acc2cbaf81969207ecb
rafal <rafal@rafal.comm>
2021-01-09 21:06:00 +0100

    fix: GEOT-777: GEOT-777: small changes css

--- packages/ui/NotificationNewMember/index.tsx
+++ packages/ui/NotificationNewMember/index.tsx
@@ -13,7 +13,7 @@ export const NotificationNewMember: FC<{
 }> = ({ group, member }) => {
   return (
     <li className={styles.wrapper} data-testid={'notification-new-member'}>
-      <Link href={`/member/${member._id}`}>
+      <Link href={`/member/${member.id}`}>
         <a>
           <Picture
             additionalStyles={styles.avatar}
