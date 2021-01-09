06db1d93470e444d9644b2bc5fa1182e8188fe57
rafal <rafal@rafal.comm>
2021-01-09 21:28:32 +0100

    fix: GEOT-777: small changes css

--- packages/app-andrew/src/components/group/PromotedTags/index.tsx
+++ packages/app-andrew/src/components/group/PromotedTags/index.tsx
@@ -112,7 +112,6 @@ export const PromotedTags: FC<{
             onNoRecordsClick={onAddTagClick}
             onTagClick={onTagClick}
             tags={state.tags}
-            type="vertical"
           />
         ))}
       {promotedTagEditor.isOpen && (
