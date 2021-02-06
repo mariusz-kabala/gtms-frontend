291d07eaa4d65d668689ce567eca450860b411ae
rafal <rafal@rafal.comm>
2021-02-06 23:28:11 +0100

    feat: GEOT-698-better cookie component

--- packages/ui/CookiePolicy/index.tsx
+++ packages/ui/CookiePolicy/index.tsx
@@ -2,7 +2,12 @@ import React, { FC, useState } from 'react'
 import cx from 'classnames'
 import { getItem, setItem } from '@gtms/commons/helpers/localStorage'
 // ui
-import { IoIosArrowDown, IoIosArrowForward, IoIosCloseCircle, IoIosCheckbox } from 'react-icons/io'
+import {
+  IoIosArrowDown,
+  IoIosArrowForward,
+  IoIosCloseCircle,
+  IoIosCheckbox,
+} from 'react-icons/io'
 import { BsArrow90DegUp } from 'react-icons/bs'
 import { FaIdCard } from 'react-icons/fa'
 import { Scrollbars } from 'react-custom-scrollbars'
@@ -34,19 +39,16 @@ export const CookiePolicy: FC = () => {
       arr.push(
         <div className={styles.switcherWrapper}>
           <div className={styles.switcher}>
-            <SwitchWrapper
-              onChange={() => null}
-              checked={false}
-            />
+            <SwitchWrapper onChange={() => null} checked={false} />
             <label>Group is public</label>
           </div>
           <span>
             <i>
               <BsArrow90DegUp />
             </i>
-            If group is public, anyone can find it, and read its content. If
-            not - only members can do that
-          </span>                  
+            If group is public, anyone can find it, and read its content. If not
+            - only members can do that
+          </span>
         </div>
       )
     }
@@ -70,8 +72,13 @@ export const CookiePolicy: FC = () => {
           <Scrollbars style={{ width: '100%', height: '100%' }}>
             <div className={styles.content}>
               {renderSwitchers(10)}
-              <h2 className={styles.header}><span>Festiwale</span>Muzyczne.pl</h2>
-              <span className={styles.subHeader}>FestiwaleMuzyczne.pl prosi Cię o zgodę na wykorzystanie Twoich danych osobowych w następujących celach:</span>
+              <h2 className={styles.header}>
+                <span>Festiwale</span>Muzyczne.pl
+              </h2>
+              <span className={styles.subHeader}>
+                FestiwaleMuzyczne.pl prosi Cię o zgodę na wykorzystanie Twoich
+                danych osobowych w następujących celach:
+              </span>
               <ul className={styles.list}>
                 <li className={styles.item}>
                   <i>
@@ -86,42 +93,59 @@ export const CookiePolicy: FC = () => {
                   Labore mollit enim qui magna id elit nisi aliqua in.
                 </li>
               </ul>
-              <h3 className={styles.expandingHeader} onClick={() => setIsExpanded(!isExpanded)}>
+              <h3
+                className={styles.expandingHeader}
+                onClick={() => setIsExpanded(!isExpanded)}
+              >
                 More info
-                {isExpanded && <i><IoIosArrowDown /></i>}
-                {!isExpanded && <i><IoIosArrowForward /></i>}
+                {isExpanded && (
+                  <i>
+                    <IoIosArrowDown />
+                  </i>
+                )}
+                {!isExpanded && (
+                  <i>
+                    <IoIosArrowForward />
+                  </i>
+                )}
               </h3>
               {isExpanded && (
-              <ul className={cx(styles.list, styles.expanding)}>
-                <li className={styles.item}>
-                  Do elit proident culpa laboris cillum irure magna veniam.
-                </li>
-                <li className={styles.item}>
-                  Do elit proident culpa laboris cillum irure magna veniam.
-                </li>
-                <li className={styles.item}>
-                  Do elit proident culpa laboris cillum irure magna veniam.
-                </li>
-                <li className={styles.item}>
-                  Do elit proident culpa laboris cillum irure magna veniam.
-                </li>
+                <ul className={cx(styles.list, styles.expanding)}>
+                  <li className={styles.item}>
+                    Do elit proident culpa laboris cillum irure magna veniam.
+                  </li>
+                  <li className={styles.item}>
+                    Do elit proident culpa laboris cillum irure magna veniam.
+                  </li>
+                  <li className={styles.item}>
+                    Do elit proident culpa laboris cillum irure magna veniam.
+                  </li>
+                  <li className={styles.item}>
+                    Do elit proident culpa laboris cillum irure magna veniam.
+                  </li>
                 </ul>
               )}
               <div className={styles.text}>
                 <p>
-                Twoje dane osobowe będą przetwarzane, a informacje z Twojego urządzenia (pliki cookie, unikalne identyfikatory itp.) mogą być wyświetlane i zapisywane przez zewnętrznych dostawców lub im udostępniane. Mogą też być wykorzystywane przez tę witrynę lub aplikację.
+                  Twoje dane osobowe będą przetwarzane, a informacje z Twojego
+                  urządzenia (pliki cookie, unikalne identyfikatory itp.) mogą
+                  być wyświetlane i zapisywane przez zewnętrznych dostawców lub
+                  im udostępniane. Mogą też być wykorzystywane przez tę witrynę
+                  lub aplikację.
                 </p>
                 <p>
-                  Niektórzy dostawcy mogą przetwarzać Twoje dane osobowe na podstawie uzasadnionego interesu. Możesz się na to nie zgodzić, zmieniając opcje poniżej. W każdej chwili możesz wycofać swoją zgodę na naszej stronie z Polityką prywatności.
+                  Niektórzy dostawcy mogą przetwarzać Twoje dane osobowe na
+                  podstawie uzasadnionego interesu. Możesz się na to nie
+                  zgodzić, zmieniając opcje poniżej. W każdej chwili możesz
+                  wycofać swoją zgodę na naszej stronie z Polityką prywatności.
                 </p>
-              </div>            
+              </div>
             </div>
           </Scrollbars>
         </div>
       </div>
       <div className={styles.buttons}>
         <Button
-          data-testid="action-button"
           additionalStyles={styles.btn}
           onClick={() => {
             setIsHiddenCompletely(true)
@@ -130,11 +154,11 @@ export const CookiePolicy: FC = () => {
         >
           <i>
             <IoIosCloseCircle />
-          </i>            
+          </i>
           Options
         </Button>
         <Button
-          data-testid="action-button"
+          testid="action-button"
           additionalStyles={styles.btn}
           onClick={() => {
             setIsHiddenCompletely(true)
@@ -146,7 +170,7 @@ export const CookiePolicy: FC = () => {
           </i>
           Accept
         </Button>
-      </div>       
+      </div>
       {isActive && !isHiddenCompletely && (
         <Overlay onClick={() => setIsActive(false)} />
       )}
