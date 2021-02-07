40abcf1a12dbbe28d3d30386e6ba9fe49ef28bf6
ra8ga <rafal.furmaga@gmail.com>
2021-02-07 08:11:07 +0100

    feat: GEOT-698: better cookie component (#926)
    * feat: GEOT-698: GEOT-698-better-cookie-accept
    
    * feat: GEOT-698: GEOT-698-better-cookie-accept
    
    Co-authored-by: rafal <rafal@rafal.comm>

--- packages/app-andrew/src/pages/_app.tsx
+++ packages/app-andrew/src/pages/_app.tsx
@@ -9,9 +9,9 @@ import { NotificationsActive } from '@app/components/commons/NotificationsActive
 import { GroupPreview } from '@app/components/commons/GroupPreview'
 import { init, initAuthSession } from '@gtms/state-user'
 import { init as initWPN } from '@gtms/state-notification'
+import { CookiePolicy } from '@gtms/ui/CookiePolicy'
 import { LoginWindow } from '@app/components/commons/LoginWindow'
 import { uiQuery } from '@app/state'
-
 import '@gtms/styles/scss/global.scss'
 import './tooltip.scss'
 import './lightbox.scss'
@@ -103,6 +103,7 @@ class GTMSApp extends App<GTMSAppProps, {}, GTMSAppState> {
         <Head>
           <link rel="manifest" href="/manifest.json" />
         </Head>
+        <CookiePolicy />
         <LoginWindow />
         <NotificationsActive />
         <NotificationsSidebar />
